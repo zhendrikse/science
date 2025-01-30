efedf2c8e0be02d19758f32e612d584fcf873ed2
Zeger Hendrikse <zegerh@yahoo.co.uk>
2025-01-31 00:23:01 +0100

    Typo in latex

--- mathematics/glowscript/multivariate_functions.py
+++ mathematics/glowscript/multivariate_functions.py
@@ -35,7 +35,7 @@ np = Numpy()
 #############
 
 
-ricker_title = "<h3<a href=\"https://en.wikipedia.org/wiki/Ricker_wavelet\">Ricker / Mexican hat / Marr wavelet</a></h3>$F(x,y) = \\dfrac{1}{\pi\sigma^4} \\bigg(1 - \dfrac{1}{2} \\bigg( \dfrac{x^2 + y^2}{\sigma^2} \\bigg) \\bigg) e^{-\\dfrac{x^2+y^2}{2\sigma^2}}$"
+ricker_title = "<h3><a href=\"https://en.wikipedia.org/wiki/Ricker_wavelet\">Ricker / Mexican hat / Marr wavelet</a></h3>&nbsp;&nbsp;$F(x,y) = \\dfrac{1}{\pi\sigma^4} \\bigg(1 - \dfrac{1}{2} \\bigg( \dfrac{x^2 + y^2}{\sigma^2} \\bigg) \\bigg) e^{-\\dfrac{x^2+y^2}{2\sigma^2}}$"
 sine_cosine_title = "<h3>$F(x, y) = \\sin(\\pi x)\\cos(\\pi y)$</h3>"
 sine_sqrt_title = "<h3>$F(x, y) = \\sqrt{x^2+y^2}$</h3>"
 polynomial_title = "<h3>$F(x, y) =  (yx^3 - xy^3)$</h3>"
