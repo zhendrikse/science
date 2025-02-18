fdfc7182d20b8c6f90869c6159578c6976a49314
Zeger Hendrikse <zegerh@yahoo.co.uk>
2025-02-18 14:21:11 +0100

    Added Mandelbrot.

--- mathematics/code/mandelbrot.py
+++ mathematics/code/mandelbrot.py
@@ -1,5 +1,5 @@
 #Web VPython 3.2
-from vpython import vector, canvas, points, color, rate, radio
+from vpython import vector, canvas, points, color, rate, radio, button
 
 title = """&#x2022; Based on the original <a href="https://vpython.org/contents/contributed/pixelplot.py">pixelplot.py</a> by Bruce Sherwood, Jan. 1, 2008
 &#x2022; Refactored by <a href="https://www.hendrikse.name/">Zeger Hendrikse</a> in <a href="https://github.com/zhendrikse/science/blob/main/mathematics/code/mandelbrot.py">mandelbrot.py</a>
@@ -16,14 +16,16 @@ display = create_canvas(resolution)
 display.title = title
 
 class RadioButton:
-    def __init__(self, button_, function_):
+    def __init__(self, button_, function_, max_iterations):
         self._button = button_
         self._function = function_
+        self._max_iterations = max_iterations
 
     def uncheck(self):
         self._button.checked = False
 
     def push(self):
+        mandelbrot.set_max_iterations_to(self._max_iterations)
         mandelbrot.render(self._function)
 
     def check(self):
@@ -38,8 +40,8 @@ class RadioButtons:
         self._radio_buttons = []
         self._selected_button = None
 
-    def add(self, button_, function_):
-        self._radio_buttons.append(RadioButton(button_, function_))
+    def add(self, button_, function_, max_iterations):
+        self._radio_buttons.append(RadioButton(button_, function_, max_iterations))
 
         if (len(self._radio_buttons) % 5) == 0:
             display.append_to_caption("\n\n")
@@ -65,9 +67,9 @@ class RadioButtons:
         return self._selected_button.name()
 
 class Mandelbrot:
-    def __init__(self):
+    def __init__(self, max_iterations=100):
         self._pixels = points()
-        self._max_iteration = 100
+        self._max_iteration = max_iterations
 
     def set_color_for_pixel_at(self, x, y, colour):
         self._pixels.append(pos=vector(x, y, 0), color=colour)
@@ -98,19 +100,22 @@ class Mandelbrot:
                     colour = color.hsv_to_rgb(vector(iterations / self._max_iteration - .1, 1, 1))
                     self.set_color_for_pixel_at(nx, ny, colour)
 
+    def set_max_iterations_to(self, max_iterations):
+        self._max_iteration = max_iterations
+
 
 def complex_(re, im):
     return re, im
 
-def set_1(x, y):
+def fractal_1(x, y):
     # from -2 to 1
-    z = complex_(-2 + x * 3 / resolution, -2 + y * 3 / resolution)
-    c = complex_(-2 + x * 3 / resolution, -2 + y * 3 / resolution)
+    z = complex_(-2.15 + x * 3 / resolution, -1.75 + y * 3 / resolution)
+    c = complex_(-2.15 + x * 3 / resolution, -1.75 + y * 3 / resolution)
     return z, c
 
-def set_2(x, y):
-    z = complex_(-1.5 + x * 3 / resolution, -2 + y * 3 / resolution)
-    c = complex_(0.0019 * (z[0] - resolution / 2), 0.0019 * (z[1] - resolution / 2))
+def fractal_2(x, y):
+    z = complex_(-1.5 + x / 200, -1.5 + y / 200)
+    c = complex_(0.0019 * (z[0] - 300), 0.0019 * (z[1] - 300))
     return z, c
 
 def toggle(event):
@@ -119,15 +124,20 @@ def toggle(event):
     display = create_canvas(resolution)
     radio_buttons.toggle(event.name)
 
+def download():
+    display.capture("fractal")
+
 radio_buttons = RadioButtons()
-radio_buttons.add(radio(bind=toggle, text="Set 1", name="set_1"), set_1)
-radio_buttons.add(radio(bind=toggle, text="Set 2", name="set_2"), set_2)
+radio_buttons.add(radio(bind=toggle, text="Fractal 1 ", name="1"), fractal_1, 100)
+radio_buttons.add(radio(bind=toggle, text="Fractal 1a ", name="1a"), fractal_1, 25)
+radio_buttons.add(radio(bind=toggle, text="Fractal 2   ", name="2"), fractal_2, 100)
+_ = button(text="Download", bind=download, align="right")
 
 display.delete()
 display = create_canvas(resolution)
 
-mandelbrot = Mandelbrot()
-mandelbrot.render(set_1)
+mandelbrot = Mandelbrot(100)
+mandelbrot.render(fractal_1)
 
 while True:
     rate(20)
--- mathematics/mandelbrot.md
+++ mathematics/mandelbrot.md
@@ -1,7 +1,19 @@
 {% include breadcrumbs.html %}
 
-## Generation of a Mandelbrot images using VPython
+## Generating Mandelbrot images using VPython
 <div class="header_line"><br/></div>
+
+The term fractal was coined by [Benoit Mandelbrot](https://en.wikipedia.org/wiki/Benoit_Mandelbrot) 
+from the Latin ‘fractus’, which means broken or fractured.
+
+<blockquote>
+Because of his access to IBM's computers, Mandelbrot was one of the first to use computer 
+graphics to create and display fractal geometric images, leading to his discovery of the 
+Mandelbrot set in 1980. He showed how visual complexity can be created from simple rules. 
+He said that things typically considered to be "rough", a "mess", or "chaotic", 
+such as clouds or shorelines, actually had a "degree of order". &mdash; 
+<a href="https://en.wikipedia.org/wiki/Benoit_Mandelbrot">Wikipedia</a>
+</blockquote><br/>
 
 {% include_relative code/Mandelbrot.html %}
 
