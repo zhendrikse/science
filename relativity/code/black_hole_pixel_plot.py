e6f5377d794726e0e0727b907778d0bbf0157ca3
Zeger Hendrikse <zegerh@yahoo.co.uk>
2025-02-20 11:58:23 +0100

    Layout.

--- relativity/code/black_hole_pixel_plot.py
+++ relativity/code/black_hole_pixel_plot.py
@@ -191,6 +191,7 @@ disk = Disk(c_focus, 4.5 * bh.radius, 16.2 * bh.radius, colour=vector(0.75, 0.15
 
 camera_ = Camera(c_origin, c_focus - c_origin, 1.2)
 scene = Scene(width=x_resolution, height=x_resolution // 2, camera=camera_, blackhole=bh, disk=disk)
+MathJax.Hub.Queue(["Typeset", MathJax.Hub])
 
 engine = Engine(scene)
 engine.render()
