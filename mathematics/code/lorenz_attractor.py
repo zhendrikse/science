d2a50b6d3411352190545b6c01ba90002b0c9fe0
Zeger Hendrikse <zegerh@yahoo.co.uk>
2025-02-25 10:53:01 +0100

    Updated Lorenz attractor.

--- mathematics/code/lorenz_attractor.py
+++ mathematics/code/lorenz_attractor.py
@@ -1,13 +1,13 @@
 #Web VPython 3.2
 
-from vpython import simple_sphere, canvas, curve, vec, color, sqrt, rate, arange, cylinder, label, box, pi, arrow, checkbox
+from vpython import simple_sphere, canvas, curve, vec, color, text, rate, arange, cylinder, box, pi, arrow, checkbox, radio
 
 title = """&#x2022; Based on <a href="https://prettymathpics.com/lorenz-attractor/">Lorenz Attractor</a> blog and code contained therein
 &#x2022; Refactored and extended to <a href="https://github.com/zhendrikse/science/blob/main/mathematics/code/lorenz_attractor.py">lorenz_attractor.py</a> by <a href="https://www.hendrikse.name/">Zeger Hendrikse</a>
 
 """
 
-display = canvas(range=47, title=title, width=600, height=600, background=color.gray(0.075), forward=vec(-.14, -.2, -1))
+display = canvas(range=52, title=title, width=600, height=600, background=color.gray(0.075), forward=vec(-.14, -.2, -1))
 
 x_hat = vec(1, 0, 0)
 y_hat = vec(0, 1, 0)
@@ -33,12 +33,12 @@ class Base:
             pos_x_y = x_hat * x_[0] + y_hat * y_[0]
             pos_x_z = x_hat * x_[0] + z_hat * z_[0]
             pos_y_z = y_hat * y_[0] + z_hat * z_[0]
-            self._xy_mesh += [cylinder(pos=position + pos_x_y + x_hat * j * delta_x, axis=y_hat * range_y, color=color.gray(.5), radius=scale * .75, visible=False)]
-            self._xy_mesh += [cylinder(pos=position + pos_x_y + y_hat * j * delta_y, axis=x_hat * range_x, color=color.gray(.5), radius=scale * .75, visible=False)]
-            self._xz_mesh += [cylinder(pos=position + pos_x_z + x_hat * j * delta_x, axis=z_hat * range_z, color=color.gray(.5), radius=scale * .75, visible=False)]
-            self._xz_mesh += [cylinder(pos=position + pos_x_z + z_hat * j * delta_z, axis=x_hat * range_x, color=color.gray(.5), radius=scale * .75, visible=False)]
-            self._yz_mesh += [cylinder(pos=position + pos_y_z + y_hat * j * delta_y, axis=z_hat * range_z, color=color.gray(.5), radius=scale * .75, visible=False)]
-            self._yz_mesh += [cylinder(pos=position + pos_y_z + z_hat * j * delta_z, axis=y_hat * range_y, color=color.gray(.5), radius=scale * .75, visible=False)]
+            self._xy_mesh += [cylinder(pos=position + pos_x_y + x_hat * j * delta_x, axis=y_hat * range_y, color=color.gray(.6), radius=scale * .75, visible=False)]
+            self._xy_mesh += [cylinder(pos=position + pos_x_y + y_hat * j * delta_y, axis=x_hat * range_x, color=color.gray(.6), radius=scale * .75, visible=False)]
+            self._xz_mesh += [cylinder(pos=position + pos_x_z + x_hat * j * delta_x, axis=z_hat * range_z, color=color.gray(.6), radius=scale * .75, visible=False)]
+            self._xz_mesh += [cylinder(pos=position + pos_x_z + z_hat * j * delta_z, axis=x_hat * range_x, color=color.gray(.6), radius=scale * .75, visible=False)]
+            self._yz_mesh += [cylinder(pos=position + pos_y_z + y_hat * j * delta_y, axis=z_hat * range_z, color=color.gray(.6), radius=scale * .75, visible=False)]
+            self._yz_mesh += [cylinder(pos=position + pos_y_z + z_hat * j * delta_z, axis=y_hat * range_y, color=color.gray(.6), radius=scale * .75, visible=False)]
 
     def _make_axis(self, x_, y_, z_, delta_x, delta_y, delta_z, axis_color, axis_labels, scale):
         c1 = cylinder(pos=x_hat * x_[0], axis=x_hat * (x_[-1] - x_[0]), color=axis_color, radius=scale)
@@ -47,9 +47,9 @@ class Base:
         a1 = arrow(pos=x_hat * x_[-1], color=axis_color, shaftwidth=scale * 2, axis=delta_x * x_hat, round=True)
         a2 = arrow(pos=y_hat * y_[-1], color=axis_color, shaftwidth=scale * 2, axis=delta_y * y_hat, round=True)
         a3 = arrow(pos=z_hat * z_[-1], color=axis_color, shaftwidth=scale * 2, axis=delta_z * z_hat, round=True)
-        l1 = label(pos=x_hat * (x_[-1] + 1.5 * delta_x) - vec(0, scale, 0), text=axis_labels[0], color=axis_color, height=scale * 200, box=False)
-        l2 = label(pos=y_hat * (y_[-1] + 1.5 * delta_y) - vec(0, scale, 0), text=axis_labels[1], color=axis_color, height=scale * 200, box=False)
-        l3 = label(pos=z_hat * (z_[-1] + 1.5 * delta_z) - vec(0, scale, 0), text=axis_labels[2], color=axis_color, height=scale * 200, box=False)
+        l1 = text(pos=x_hat * (x_[-1] + 1.25 * delta_x) - vec(0, scale, 0), text=axis_labels[0], color=axis_color, height=scale * 15, billboard=True)
+        l2 = text(pos=y_hat * (y_[-1] + 1.25 * delta_y) - vec(0, scale, 0), text=axis_labels[1], color=axis_color, height=scale * 15, billboard=True)
+        l3 = text(pos=z_hat * (z_[-1] + 1.25 * delta_z) - vec(0, scale, 0), text=axis_labels[2], color=axis_color, height=scale * 15, billboard=True)
         self._axis += [c1, c2, c3, a1, a2, a3, l1, l2, l3]
 
     def _make_tick_marks(self, x_dim, y_dim, z_dim, tick_marks_color, scale):
@@ -83,6 +83,50 @@ class Base:
         for i in range(len(self._xz_mesh)):
             self._yz_mesh[i].visible = visible
 
+class LorenzAttractor:
+    def __init__(self):
+        self._lines, self._dots = [], []
+
+    def hide_lines(self):
+        for line_ in self._lines:
+            line_.visible = False
+
+    def hide_dots(self):
+        for dot_ in self._dots:
+            dot_.visible = False
+
+    def show_lines(self):
+        for line in self._lines:
+            line.visible = True
+
+    def show_dots(self):
+        for dot_ in self._dots:
+            dot_.visible = True
+
+    def _color(self, skip, dist):
+        dmaxx = 1.90274198766  # get this from print max_distance
+        hue = saturation = value = 0
+        if skip > 100:
+            hue, saturation, value = dist / dmaxx, 1, 1
+        return color.hsv_to_rgb(vec(hue, saturation, value))
+
+    def generate(self, a, b, c, N=150, h=0.004):
+        old_pos = vec(0.1, 0.0, 0.0)
+        skip = max_distance = dist = 0
+        for i in range(N):
+            self._lines += [curve(pos=old_pos, color=self._color(skip, dist), radius=0.1, visible=False)]
+            for j in range(N):
+                x = old_pos.x + h * a * (old_pos.y - old_pos.x)
+                y = old_pos.y + h * (old_pos.x * (b - old_pos.z) - old_pos.y)
+                z = old_pos.z + h * (old_pos.x * old_pos.y - c * old_pos.z)
+                new_pos = vec(x, y, z)
+
+                dist = (new_pos - old_pos).mag
+                if dist > max_distance: max_distance = dist
+                self._lines[-1].append(pos=new_pos, color=self._color(skip, dist))
+                self._dots += [simple_sphere(pos=new_pos, color=self._color(skip, dist), radius=0.35, visible=False)]
+                skip += 1
+                old_pos = new_pos
 
 def toggle_tick_marks(event):
     axis.tick_marks_visibility_is(event.checked)
@@ -104,54 +148,71 @@ def toggle_axis(event):
     axis.axis_visibility_is(event.checked)
 
 
-display.append_to_caption("\n")
-_ = checkbox(text='Tick marks', bind=toggle_tick_marks, checked=False)
-_ = checkbox(text='YZ mesh', bind=toggle_yz_mesh, checked=False)
-_ = checkbox(text='XZ mesh', bind=toggle_xz_mesh, checked=False)
-_ = checkbox(text='XY mesh', bind=toggle_xy_mesh, checked=False)
-_ = checkbox(text='Axis', bind=toggle_axis, checked=True)
+def toggle_line_type(event):
+    global current_lorenz
+    if event.name == "dots":
+        current_lorenz.show_dots()
+        current_lorenz.hide_lines()
+        radio_1.checked = False
+    if event.name == "lines":
+        current_lorenz.show_lines()
+        current_lorenz.hide_dots()
+        radio_2.checked = False
 
+
+def toggle_a_b_c(event):
+    global current_lorenz
+    current_lorenz.hide_lines()
+    current_lorenz.hide_dots()
+    if event.name == "1":
+        current_lorenz = lorenz_1
+        radio_4.checked = False
+        display.range = 52
+    if event.name == "2":
+        current_lorenz = lorenz_2
+        radio_3.checked = False
+        display.range = 84
+    current_lorenz.show_lines()
+    radio_1.checked = True
+    radio_2.checked = False
+
+def toggle_background(event):
+    display.background = color.gray(0.075) if display.background == color.gray(0.75) else color.gray(0.75)
+
+display.append_to_caption("\n")
+_ = checkbox(text='Tick marks ', bind=toggle_tick_marks, checked=False)
+_ = checkbox(text='YZ-mesh ', bind=toggle_yz_mesh, checked=False)
+_ = checkbox(text='XZ-mesh ', bind=toggle_xz_mesh, checked=False)
+_ = checkbox(text='XY-mesh ', bind=toggle_xy_mesh, checked=False)
+_ = checkbox(text='Axis', bind=toggle_axis, checked=True)
+_ = checkbox(text='Dark background', bind=toggle_background, checked=True)
+display.append_to_caption("\n\n")
+radio_1 = radio(text="Lines ", checked=True, name="lines", bind=toggle_line_type)
+radio_2 = radio(text="Dots ", checked=False, name="dots", bind=toggle_line_type)
+display.append_to_caption("\n\n")
+radio_3 = radio(text="a, b, c = 10, 28, 8/3 ", checked=True, name="1", bind=toggle_a_b_c)
+display.append_to_caption("\n\n")
+radio_4 = radio(text="a, b, c = 28, 46.92, 4", checked=False, name="2", bind=toggle_a_b_c)
 
 def linspace(start, stop, num):
     return [x for x in arange(start, stop, (stop - start) / (num - 1))] + [stop]
 
 
-space = Space(linspace(-30, 30, 11), linspace(-30, 30, 11), linspace(0, 50, 11))
-axis = Base(space, axis_color=vec(0.8, 0.8, 0))
+space = Space(linspace(-35, 35, 11), linspace(-35, 35, 11), linspace(0, 50, 11))
+axis = Base(space, axis_color=vec(0.7, 0.7, 0.7))
 
+lorenz_1 = LorenzAttractor()
+lorenz_1.generate(10.0, 28.0, 8.0 / 3.0)
+lorenz_1.show_lines()
 
-hue_increment = .00005  # hue increment
-dmaxx = 1.90274198766  # get this from print max_distance
-N = 150
-h = 0.004
+lorenz_2 = LorenzAttractor()
+lorenz_2.generate(28, 46.92, 4.)
 
-a, b, c = 10.0, 28.0, 8.0 / 3.0
-#a, b, c = 28, 46.92, 4.
+current_lorenz = lorenz_1
 
-x0 = 0.1
-y0 = 0.0
-z0 = 0.0
-H = S = V = 0
-max_distance = 0
-skip = 0
-for i in range(N):
-    line = curve(pos=vec(x0, y0, z0), color=color.hsv_to_rgb(vec(H, S, V)), radius=0.1)
-    for j in range(N):
-        x1 = x0 + h * a * (y0 - x0)
-        y1 = y0 + h * (x0 * (b - z0) - y0)
-        z1 = z0 + h * (x0 * y0 - c * z0)
-        dist = sqrt((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0) + (z1 - z0) * (z1 - z0))
-        if dist > max_distance: max_distance = dist
-        x0 = x1
-        y0 = y1
-        z0 = z1
-        if skip > 100:
-            H = dist / dmaxx
-            S = V = 1
-        line.append( pos=vec(x0,y0,z0), color=color.hsv_to_rgb(vec(H,S,V)))
-        #simple_sphere(pos=vec(x0, y0, z0), color=color.hsv_to_rgb(vec(H, S, V)), radius=0.3)
-        skip += 1
-
+#################################
+# COMMENT OUT IN LOCAL VPYTHON  #
+# MathJax.Hub.Queue(["Typeset", MathJax.Hub])
 #print(max_distance)
 while True:
     rate(10)
