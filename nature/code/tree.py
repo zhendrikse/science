f5cad1b17f76cde2f996a2e5ab86b4c105836850
Zeger Hendrikse <zegerh@yahoo.co.uk>
2025-02-27 16:36:25 +0100

    Update.

--- nature/code/tree.py
+++ nature/code/tree.py
@@ -1,43 +1,44 @@
-title = """&#x2022; Based on <a href="http://rosettacode.org/wiki/Fractal_tree#Python">Fractal_tree</a> via <a href="https://prettymathpics.com/fractal-tree/">prettymathpics.com</a> 
-&#x2022; Original authors: Alan Richmond, Python3.codes, and others at <a href="http://rosettacode.org/">rosettacode.org</a>
-&#x2022; Adapted to <a href="https://vpython.org/">VPython</a> by <a href="https://www.hendrikse.name/">Zeger Hendrikse</a> in <a href="https://github.com/zhendrikse/science/blob/main/nature/code/tree.py">tree.py</a>
+#Web VPython 3.2
+
+from vpython import vector, rotate, color, vec, sphere, cylinder, canvas, textures, pi, rate, random
+
+title = """&#x2022; Based on <a href="https://trinket.io/glowscript/9d17ee2036">this example</a> 
+&#x2022; Modified by <a href="https://www.hendrikse.name/">Zeger Hendrikse</a> in <a href="https://github.com/zhendrikse/science/blob/main/nature/code/tree.py">tree.py</a>
 
 """
 
-from vpython import curve, cos, radians, color, vec, sin, rate, canvas
+display = canvas(background=color.gray(0.075), center=vector(0, 50, 0), height=500, title=title)
 
-display = canvas(title=title, width=600, height=400, range=380, center=vec(300, -18, -70), forward=vec(-.45, -.2, -.87), background=color.gray(0.075))
+def tree(branch_length, branch_pos, branch_vector):
+    theta_0 = 30 * pi / 180 # theta is the "bend angle" for each branching
+    short = 14.5  # this is the amount each branch is decreased by
 
-spread = 17  # how much branches spread apart
-width, height = 512, 400  # window size
-max_recursion_depth = 12  # maximum recursion depth
-branch_length = 8.0  # branch length factor
+    # repeat the branching until the length is shorter than 5
+    if branch_length > 5:
+        theta = theta_0 # + (random() - .5) * pi / 10
 
-lines = [curve(pos=vec(width / 2, height * 0.9, 0))]  # , radius=0.1, visible=False)]
+        if branch_length < 20:
+            sphere(pos=branch_pos, axis=branch_vector * branch_length, radius=1.5 * branch_length, color=vec(0, .5, 0))
+        else:
+            cylinder(pos=branch_pos, axis=branch_vector * branch_length, radius=.1 * branch_length, texture=textures.wood)
+        branch_pos += branch_vector * branch_length
+        # rotate turns the pointing direction
+        branch_vector = rotate(branch_vector, angle=theta, axis=vector(0, 0, 1))
+        # here is the recursive magic
+        tree(branch_length - short, branch_pos, branch_vector)
 
+        # now you have to go back to where you were
+        branch_vector = rotate(branch_vector, angle=-2 * theta, axis=vector(0, 0, 1))
 
-# This function calls itself to add sub-trees
-def draw_tree(x1, y1, angle, depth):
-    if depth <= 0:
-        return
+        # this does the other side (also recursively)
+        tree(branch_length - short, branch_pos, branch_vector)
+        branch_vector = rotate(branch_vector, angle=theta, axis=vector(0, 0, 1))
+        branch_pos -= branch_length * branch_vector
 
-    # compute this branch's next endpoint
-    x2 = x1 + int(cos(radians(angle)) * depth * branch_length)
-    y2 = y1 + int(sin(radians(angle)) * depth * branch_length)
+# Artificially elongate the stem
+stem = cylinder(pos=vec(0, -300, 0), radius=12.5, axis=vec(0, 175, 0), texture=textures.wood)
 
-    # https://docs.python.org/2/library/colorsys.html
-    colour = color.hsv_to_rgb(vec(float(depth) / max_recursion_depth, 1.0, 1.0))
-
-    # draw the branch
-    lines.append(curve(pos=[vec(x1, -y1, 0), vec(x2, -y2, 0)], color=colour))
-
-    # and append 2 trees by recursion
-    draw_tree(x2, y2, angle - spread, depth - 1)
-    draw_tree(x2, y2, angle + spread, depth - 1)
-
-
-#   Start drawing!
-draw_tree(width / 2, height * 0.9, -90, max_recursion_depth)
+tree(branch_length=125, branch_pos=vector(0, -125, 0), branch_vector=vector(0, 1, 0))
 
 while True:
-    rate(10)
+    rate(2)
