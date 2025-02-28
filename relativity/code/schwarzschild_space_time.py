c0e4716fb0953a1fe5ba171dec0c486b7eb3ee69
Zeger Hendrikse <zegerh@yahoo.co.uk>
2025-02-28 19:20:19 +0100

    Update.

--- relativity/code/schwarzschild_space_time.py
+++ relativity/code/schwarzschild_space_time.py
@@ -1,7 +1,7 @@
 # Web VPython 3.2
-from vpython import curve, vec, cos, pi, sqrt, sin, color, rate, sphere, slider, canvas
+from vpython import curve, vec, cos, pi, sqrt, sin, color, rate, sphere, slider, canvas, checkbox
 
-title = """&#x2022; Original <a href="https://www.glowscript.org/#/user/Luinthoron/folder/English/program/embedding-diagram">code.py</a> by M. Ryston (Department of Physics Education)
+title = """&#x2022; Original <a href="https://www.glowscript.org/#/user/Luinthoron/folder/English/program/embedding-diagram">idea &amp; code</a> by M. Ryston (Department of Physics Education)
 &#x2022; Faculty of Mathematics and Physics, Charles University,Prague, Czech Republic
 &#x2022; Described in <a href="https://iopscience.iop.org/article/10.1088/1742-6596/1286/1/012049">Interactive animations as a tool in teaching general relativity [...]</a>
 &#x2022; Refactored by <a href="https://www.hendrikse.name/">Zeger Hendrikse</a> in <a href="https://github.com/zhendrikse/science/blob/main/relativity/code/schwarzschild_space_time.py">schwarzschild_space_time.py</a>
@@ -21,12 +21,15 @@ class Sun:
                            texture="https://www.hendrikse.name/science/astrophysics/images/textures/sun.jpg",
                            visible=True)
 
+    def visibility_is(self, checked):
+        self._sun.visible = checked
+
 
 class Comet:
-    def __init__(self, start_position, particle_radius, colour, sour):
-        self._sphere = sphere(pos=start_position, radius=particle_radius, color=colour, visible=True, make_trail=True)
+    def __init__(self, start_position, radius, colour, sour=None):
+        self._sphere = sphere(pos=start_position, radius=radius, color=colour, visible=True, make_trail=True)
         self._sour = sour
-        self._start_sour = [sour[0], sour[1], sour[2], sour[3]]
+        self._start_sour = [sour[0], sour[1], sour[2], sour[3]] if sour is not None else None
         self._start_position = start_position
 
     def update_by(self, M, dt):
@@ -51,7 +54,22 @@ class Comet:
     def reset(self):
         self._sphere.clear_trail()
         self._sphere.pos = self._start_position
-        self._sour = [self._start_sour[0], self._start_sour[1], self._start_sour[2], self._start_sour[3]]
+        self._sour = [self._start_sour[0], self._start_sour[1], self._start_sour[2], self._start_sour[3]] if self._sour is not None else None
+
+    def visibility_is(self, checked):
+        self._sphere.visible = checked
+
+    def follow(self, comet, y_offset):
+        self._sphere.pos = comet.pos()
+        self._sphere.pos.y = y_offset
+
+    def pos(self):
+        return self._sphere.pos
+
+    def leave_trail_is(self, checked):
+        if not checked:
+            self._sphere.clear_trail()
+        self._sphere.make_trail = checked
 
 
 class SchwarzschildSpaceTime:
@@ -80,6 +98,9 @@ class SchwarzschildSpaceTime:
 
     def mass(self):
         return self._mass
+
+    def grid_y_offset(self):
+        return self._grid_y_offset
 
     def z_as_function_of(self, r):
         return sqrt(8 * self._mass * r - 16 * self._mass * self._mass)
@@ -113,6 +134,14 @@ class SchwarzschildSpaceTime:
         for element in self._grid:
             element.radius = thickness
 
+    def grid_visibility_is(self, checked):
+        for line in self._grid:
+            line.visible = checked
+
+    def cone_visibility_is(self, checked):
+        for line in self._surface:
+            line.visible = checked
+
 
 schwarzschild = SchwarzschildSpaceTime(mass=5.0)
 schwarzschild.draw()
@@ -121,23 +150,50 @@ sun = Sun(position=vec(0, 2, 0), radius=2 * schwarzschild.mass())
 
 r_min, r_max = schwarzschild.r_min_r_max()
 start_r = r_max - 0.1
-comet = Comet(start_position=vec(start_r, schwarzschild.z_as_function_of(start_r), 0), particle_radius=0.8,
-              colour=color.cyan, sour=[start_r, 0, -25.2, 0.49])
+position=vec(start_r, schwarzschild.z_as_function_of(start_r), 0)
+comet = Comet(start_position=position, radius=0.8, colour=color.cyan, sour=[start_r, 0, -25.2, 0.49])
+flat_motion_comet = Comet(vec(position.x, schwarzschild.grid_y_offset(), position.z), 0.8, color.red)
 
+def toggle_grid(event):
+    schwarzschild.grid_visibility_is(event.checked)
+
+def toggle_sun(event):
+    sun.visibility_is(event.checked)
+
+def toggle_cone(event):
+    schwarzschild.cone_visibility_is(event.checked)
+
+def toggle_flat_motion(event):
+    flat_motion_comet.visibility_is(event.checked)
+    flat_motion_comet.reset()
+
+def toggle_trail(event):
+    comet.leave_trail_is(event.checked)
+    flat_motion_comet.leave_trail_is(event.checked)
+
+display.append_to_caption('\n')
+_ = checkbox(text="Cone", bind=toggle_cone, checked=True)
+_ = checkbox(text="Grid", bind=toggle_grid, checked=True)
+_ = checkbox(text="Central mass", bind=toggle_sun, checked=True)
+_ = checkbox(text="Flat motion", bind=toggle_flat_motion, checked=True)
+_ = checkbox(text="Leave trail", bind=toggle_trail, checked=True)
 
 def modify_grid_thickness(event):
     schwarzschild.modify_grid(event.value)
 
 
-display.append_to_caption('\nGrid thickness')
+display.append_to_caption('\n\nGrid thickness')
 slider(min=0.1, max=0.5, value=0.2, bind=modify_grid_thickness)
 
 
 def restart(event):
     comet.reset()
+    flat_motion_comet.reset()
 
 
 display.bind('click', restart)
+
+#MathJax.Hub.Queue(["Typeset", MathJax.Hub])
 
 delta_t = 0.01
 while True:
@@ -145,3 +201,4 @@ while True:
     r = comet.r()
     if r_min < r < r_max:
         comet.update_by(schwarzschild.mass(), delta_t)
+        flat_motion_comet.follow(comet, schwarzschild.grid_y_offset())
