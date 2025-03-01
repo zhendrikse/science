75d825ab97185bfe462acdcb3954667a4f78f328
Zeger Hendrikse <zegerh@yahoo.co.uk>
2025-03-01 13:20:42 +0100

    Update.

--- relativity/code/schwarzschild_space_time.py
+++ relativity/code/schwarzschild_space_time.py
@@ -8,9 +8,9 @@ title = """&#x2022; Original <a href="https://www.glowscript.org/#/user/Luinthor
 
 """
 
-display = canvas(title=title, width=600, height=450, background=color.gray(0.075), forward=vec(0, -.45, -.85), range=62)
-
-
+display = canvas(title=title, width=600, height=450, background=color.gray(0.075), forward=vec(0, -.75, -.85), range=80)
+stars = sphere(pos=vec(0, 0, 0), texture="https://www.hendrikse.name/science/astrophysics/images/textures/universe.jpg", radius=200, shininess=0, opacity=0.5)
+stars.rotate(angle=pi/2, axis=vec(0, 1, 0))
 # display.range = 1.3 * r_max # r_max from SchwarzschildSpaceTime
 # display.forward = vec(-0.64, -0.77, 0.0)
 # display.center = vec(-10,0,0.2)
@@ -27,12 +27,40 @@ class Sun:
 
 class Comet:
     def __init__(self, start_position, radius, colour, sour=None):
-        self._sphere = sphere(pos=start_position, radius=radius, color=colour, visible=True, make_trail=True)
+        self._sphere = sphere(pos=start_position, radius=radius, color=colour, visible=True, make_trail=True, trail_radius=.25 * radius)
         self._sour = sour
-        self._start_sour = [sour[0], sour[1], sour[2], sour[3]] if sour is not None else None
+        self._start_sour = None if sour is None else [elem for elem in sour]
         self._start_position = start_position
+        self._is_moving = False
+
+    def update_real_motion_by(self, M, y_offset, dt):
+        if not self._is_moving:
+            return
+
+        r = self._sour[1]
+        bracket = r - 2.0 * M
+        buff = [0, 0, 0, 0, 0, 0]
+        buff[1] = self._sour[1] + 0.5 * dt * self._sour[4]
+        buff[2] = self._sour[2] + 0.5 * dt * self._sour[5]
+        buff[3] = self._sour[3] - 0.5 * dt * (M / r / bracket ** 2 * self._sour[3] * self._sour[4])
+        buff[4] = self._sour[4] + 0.5 * dt * (
+                    -M * bracket / r ** 3 * self._sour[3] ** 2 + M / r / bracket * self._sour[4] ** 2 + bracket *
+                    self._sour[5] ** 2)
+        buff[5] = self._sour[5] - 0.5 * dt * 2.0 / r * self._sour[3] * self._sour[5]
+
+        self._sour[0] += dt * buff[3]
+        self._sour[1] += dt * buff[4]
+        self._sour[2] += dt * buff[5]
+        self._sour[3] += -dt * (M / r / bracket ** 2 * buff[3] * buff[4])
+        self._sour[4] += dt * (
+                    -M * bracket / r ** 3 * buff[3] ** 2 + M / r / bracket * buff[4] ** 2 + bracket * buff[5] ** 2)
+        self._sour[5] += -dt * 2.0 / r * buff[3] * buff[5]
+        self._sphere.pos = vec(self._sour[1] * cos(self._sour[2]), y_offset, self._sour[1] * sin(self._sour[2]))
 
     def update_by(self, M, dt):
+        if not self._is_moving:
+            return
+
         buff = [0, 0, 0, 0]
         buff[0] = self._sour[0] + 0.5 * dt * self._sour[2]
         buff[1] = self._sour[1] + 0.5 * dt * self._sour[3]
@@ -54,12 +82,15 @@ class Comet:
     def reset(self):
         self._sphere.clear_trail()
         self._sphere.pos = self._start_position
-        self._sour = [self._start_sour[0], self._start_sour[1], self._start_sour[2], self._start_sour[3]] if self._sour is not None else None
+        self._sour = None if self._sour is None else [elem for elem in self._start_sour]
 
     def visibility_is(self, checked):
         self._sphere.visible = checked
 
     def follow(self, comet, y_offset):
+        if not self._is_moving:
+            return
+
         self._sphere.pos = comet.pos()
         self._sphere.pos.y = y_offset
 
@@ -71,6 +102,21 @@ class Comet:
             self._sphere.clear_trail()
         self._sphere.make_trail = checked
 
+    def modify_start_position(self, value, sour):
+        if self._is_moving:
+            return
+
+        self._sphere.pos = value
+        self._start_position = value
+        self._sour = sour
+        self._start_sour = sour
+
+    def stop_movement(self):
+        self._is_moving = False
+
+    def start_movement(self):
+        self._is_moving = True
+
 
 class SchwarzschildSpaceTime:
     def __init__(self, mass, grid_y_offset=-10):
@@ -81,7 +127,7 @@ class SchwarzschildSpaceTime:
 
     def r_min_r_max(self):
         r_min = 2 * self._mass + 0.1
-        r_max = 10 * self._mass
+        r_max = 13 * self._mass
         return r_min, r_max
 
     def draw_grid(self):
@@ -90,11 +136,11 @@ class SchwarzschildSpaceTime:
             self._grid += [curve(
                 pos=[vec(-r_max + i * 2 * r_max / 19, self._grid_y_offset, -r_max),
                      vec(-r_max + i * 2 * r_max / 19, self._grid_y_offset, r_max)],
-                color=color.white, radius=0.2)]
+                color=color.green, radius=0.2)]
             self._grid += [curve(
                 pos=[vec(-r_max, self._grid_y_offset, -r_max + i * 2 * r_max / 19),
                      vec(r_max, self._grid_y_offset, -r_max + i * 2 * r_max / 19)],
-                color=color.white, radius=0.2)]
+                color=color.green, radius=0.2)]
 
     def mass(self):
         return self._mass
@@ -113,7 +159,7 @@ class SchwarzschildSpaceTime:
         for i in range(15):
             r = r_min + i * (r_max - r_min) / 14
             t = 0
-            c = curve(pos=vec(r * cos(t), self.z_as_function_of(r), r * sin(t)), color=color.green, radius=0.2)
+            c = curve(pos=vec(r * cos(t), self.z_as_function_of(r), r * sin(t)), color=color.yellow, radius=0.2)
             for j in range(200):
                 t += 2 * pi / 200
                 c.append(vec(r * cos(t), self.z_as_function_of(r), r * sin(t)))
@@ -122,7 +168,7 @@ class SchwarzschildSpaceTime:
         for i in range(12):
             phi = i * 2 * pi / 12
             r = r_min
-            c = curve(pos=vec(r * cos(phi), self.z_as_function_of(r), r * sin(phi)), color=color.green, radius=0.2)
+            c = curve(pos=vec(r * cos(phi), self.z_as_function_of(r), r * sin(phi)), color=color.yellow, radius=0.2)
             for j in range(100):
                 r += (r_max - r_min) / 100
                 c.append(vec(r * cos(phi), self.z_as_function_of(r), r * sin(phi)))
@@ -149,10 +195,12 @@ schwarzschild.draw_grid()
 sun = Sun(position=vec(0, 2, 0), radius=2 * schwarzschild.mass())
 
 r_min, r_max = schwarzschild.r_min_r_max()
-start_r = r_max - 0.1
+start_r = r_max - 5
 position=vec(start_r, schwarzschild.z_as_function_of(start_r), 0)
-comet = Comet(start_position=position, radius=0.8, colour=color.cyan, sour=[start_r, 0, -25.2, 0.49])
-flat_motion_comet = Comet(vec(position.x, schwarzschild.grid_y_offset(), position.z), 0.8, color.red)
+comet = Comet(start_position=position, radius=1.25, colour=color.cyan, sour=[start_r, 0, -25.2, 0.49])
+flat_motion_comet = Comet(vec(position.x, schwarzschild.grid_y_offset(), position.z), 1.25, color.red)
+
+#real_motion_comet = Comet(vec(start_r, schwarzschild.grid_y_offset(), 0), 1.5, color.orange, sour=[0, start_r, 0, 0, 0, 0])
 
 def toggle_grid(event):
     schwarzschild.grid_visibility_is(event.checked)
@@ -166,10 +214,12 @@ def toggle_cone(event):
 def toggle_flat_motion(event):
     flat_motion_comet.visibility_is(event.checked)
     flat_motion_comet.reset()
+    #real_motion_comet.reset()
 
 def toggle_trail(event):
     comet.leave_trail_is(event.checked)
     flat_motion_comet.leave_trail_is(event.checked)
+    #real_motion_comet.leave_trail_is(event.checked)
 
 display.append_to_caption('\n')
 _ = checkbox(text="Cone", bind=toggle_cone, checked=True)
@@ -181,7 +231,17 @@ _ = checkbox(text="Leave trail", bind=toggle_trail, checked=True)
 def modify_grid_thickness(event):
     schwarzschild.modify_grid(event.value)
 
+def modify_start_position(event):
+    flat_motion_comet.reset()
+    comet.reset()
+    #real_motion_comet.reset()
+    pos = vec(event.value, schwarzschild.z_as_function_of(event.value), 0)
+    comet.modify_start_position(pos, [pos.x, 0, -25.2, 0.49])
+    #real_motion_comet.modify_start_position(vec(pos.x, schwarzschild.grid_y_offset(), pos.z), [0, pos.x, 0, 0, 0, 0])
 
+
+display.append_to_caption('\n\nInitial radius:')
+slider(min=r_min + 0.1, max=r_max - 0.1, value=start_r, step=0.1, bind=modify_start_position)
 display.append_to_caption('\n\nGrid thickness')
 slider(min=0.1, max=0.5, value=0.2, bind=modify_grid_thickness)
 
@@ -189,6 +249,11 @@ slider(min=0.1, max=0.5, value=0.2, bind=modify_grid_thickness)
 def restart(event):
     comet.reset()
     flat_motion_comet.reset()
+    #real_motion_comet.reset()
+
+    comet.start_movement()
+    flat_motion_comet.start_movement()
+    #real_motion_comet.start_movement()
 
 
 display.bind('click', restart)
@@ -198,7 +263,13 @@ display.bind('click', restart)
 delta_t = 0.01
 while True:
     rate(1 / delta_t)
-    r = comet.r()
-    if r_min < r < r_max:
+    if r_min < comet.r() < r_max:
         comet.update_by(schwarzschild.mass(), delta_t)
         flat_motion_comet.follow(comet, schwarzschild.grid_y_offset())
+    else:
+        comet.stop_movement()
+
+    #if r_min < real_motion_comet.r() < r_max:
+    #    real_motion_comet.update_real_motion_by(schwarzschild.mass(), schwarzschild.grid_y_offset(), delta_t)
+    #else:
+    #    real_motion_comet.stop_movement()
--- relativity/schwarzschild_space_time.md
+++ relativity/schwarzschild_space_time.md
@@ -10,6 +10,49 @@
 ### Visualization explained
 <div style="border-top: 1px solid #999999"><br/></div>
 
+The application is best described in 
+[Ryston&apos;s article](https://iopscience.iop.org/article/10.1088/1742-6596/1286/1/012049) itself:
+
+
+- _Motion of marbles rolling on bend surface affected by actual gravity and friction_ &mdash; 
+  naturally, when we use a simulation, we don’t have to deal with non-ideal conditions of real world
+  experiments. Point-like particles can be made to move strictly in the surface without friction
+  and without any force acting on them, their motion being affected purely by the curvature of the
+  surface. To support the idea that the motion is restricted to the surface, it is beneficial to be able
+  to flip the whole embedding diagram upside down, a feature that is easily achieved with a
+  simulation but hardly with a real aperture.
+- _Emphasis on the two-dimensional motion_ &mdash; as we mentioned above, the only physically relevant
+  part of the motion is in the horizontal plane (𝑟, 𝜑) or (𝑥, 𝑦), if you will. With a rotatable
+  simulation, we can take advantage of a view from the top (looking in the direction of the 𝑧 axis).
+  In this view, we see purely the effect of spatial curvature on the motion. Taking advantage of
+  the simulation further, we can add a second particle which will follow the motion of the first
+  particle but only in the horizontal plane. This way we can point out the difference in the two
+  motions and how they are related. It would be most interesting to see such a setup with the
+  marbles, but one can imagine achieving that could be extremely difficult.
+- _Purely spatial curvature_ &mdash; needless to say, this is the one conceptual obstacle that we cannot
+  get rid of when using this type of embedding diagram, and we need to keep it in mind.
+  However, what we can do is add another particle moving in the horizontal plane whose motion
+  will start with the same initial conditions as the particle on the curved surface but which moves
+  according to the complete set of equations of motion for the equatorial plane of Schwarzschild
+  spacetime. This mainly means adding the time component, which substantially changes the
+  motion. This feature allows the user to, at least qualitatively, compare the “real” motion with
+  the one due to space curvature. A practical note: In order to clearly see the curvature of the
+  embedding diagram and its effect on the motion, we usually visualize a region of space that is
+  very close to the Schwarzschild horizon, which means that the real-motion particle almost
+  immediately falls inside the central object. Therefore, this feature serves only as a rough
+  comparison of the two motions.
+- _Dynamical changes of the curvature_ &mdash; lastly, let us mention a nice feature that is again possible
+  only using a computer simulation. By changing the central mass parameter 𝑀 of the curvature
+  (see equations above), we can change the curvature dynamically, even during a particles motion.
+  While this hardly corresponds to a real world situation (the central gravitating body would have
+  to lose mass while remaining spherically symmetric and without rotation), it is an interesting
+  feature enabling us to compare trajectories for different curvatures. In other words, we can show
+  the perhaps intuitive, fact that larger mass curves space around itself more, resulting in stronger
+  curving of the trajectories
+
+### Theoretical background
+<div style="border-top: 1px solid #999999"><br/></div>
+
 The Schwarzschild metric describes a gravitational field of a non-rotating 
 spherical mass (and without electric charge), see [Wikipedia](https://en.wikipedia.org/wiki/Schwarzschild_metric):
 
