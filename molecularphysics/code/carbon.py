f612761f40f9f2ac1685581edf4ea23d69d35b24
Zeger Hendrikse <zegerh@yahoo.co.uk>
2025-03-02 00:23:58 +0100

    Refactoring

--- molecularphysics/code/carbon.py
+++ molecularphysics/code/carbon.py
@@ -16,90 +16,91 @@ rad = 3.0E-11
 scale = 2.0 * spacing
 display = canvas(range=scale * .65, width=600, height=400, background=color.gray(0.075), center=vector(scale * .2, 0, 0), title=title)
 
-theta = 30. * pi / 180.0
+class Atom:
+    def __init__(self, position, mass, colour, radius, charge, momentum=vector(0., 0., 0.)):
+        self._atom = sphere(pos=position, color=colour, radius=radius)
+        self._mass = mass
+        self._charge = charge
+        self._momentum = momentum
 
-class Carbon:
+    def pos(self):
+        return self._atom.pos
+
+    def coulomb(self, E):
+        return self._charge * E
+
+    def update(self, force, timestep):
+        self._momentum += timestep * force
+        self._atom.pos += timestep * self._momentum / self._mass
+
+    def reset(self, position):
+        self._atom.pos = position
+        self._momentum = vector(0., 0., 0.)
+
+
+class Oxygen(Atom):
+    def __init__(self, position):
+        Atom.__init__(self, position=position, mass=16.0 * u, colour=color.green, radius=rad, charge=ioncharge)
+
+
+class Carbon(Atom):
+    def __init__(self, position=vector(0, 0, 0)):
+        Atom.__init__(self, position=position, mass=12 * u, colour=color.red, radius=0.8 * rad, charge=-2.0 * ioncharge)
+
+
+class CarbonDioxide:
     def __init__(self):
-        self._oxy1 = sphere()
-        self._oxy1.pos = vector(spacing * cos(theta), spacing * sin(theta), 0.)
-        self._oxy1.mass = 16.0 * u
-        self._oxy1.charge = ioncharge
-        self._oxy1.p = vector(0., 0., 0.)
-        self._oxy1.color = color.green
-        self._oxy1.radius = rad
-        #self._oxy1pos0 = self._oxy1.pos.y
+        self._theta = 30. * pi / 180.0
+        self._oxy1 = Oxygen(position=vector(spacing * cos(self._theta), spacing * sin(self._theta), 0.))
+        self._oxy2 = Oxygen(position=-1.0 * self._oxy1.pos())
+        self._carb1 = Carbon(position=vector(0., 0.0 * spacing, 0.))
 
-        self._oxy2 = sphere()
-        self._oxy2.pos = -1.0 * self._oxy1.pos
-        self._oxy2.mass = 16.0 * u
-        self._oxy2.charge = ioncharge
-        self._oxy2.p = vector(0., 0., 0.)
-        self._oxy2.color = color.green
-        self._oxy2.radius = rad
-
-        self._carb1 = sphere()
-        self._carb1.pos = vector(0., 0.0 * spacing, 0.)
-        self._carb1.mass = 12.0 * u
-        self._carb1.charge = -2.0 * ioncharge
-        self._carb1.p = vector(0., 0., 0.)
-        self._carb1.color = color.red
-        self._carb1.radius = 0.8 * rad
-
-        self._spring1 = helix(pos=self._carb1.pos, axis=self._oxy1.pos - self._carb1.pos, radius=0.3 * rad,
+        self._spring1 = helix(pos=self._carb1.pos(), axis=self._oxy1.pos() - self._carb1.pos(), radius=0.3 * rad,
                         thickness=0.1 * rad, color=color.yellow, coils=10)
 
-        self._spring2 = helix(pos=self._carb1.pos, axis=self._oxy2.pos - self._carb1.pos, radius=0.3 * rad,
+        self._spring2 = helix(pos=self._carb1.pos(), axis=self._oxy2.pos() - self._carb1.pos(), radius=0.3 * rad,
                         thickness=0.1 * rad, color=color.yellow, coils=10)
 
     def reset(self):
-        self._oxy1.pos = vector(spacing * cos(theta), spacing * sin(theta), 0.)
-        self._oxy2.pos = -1.0 * self._oxy1.pos
-        self._carb1.pos = vector(0., 0.0 * spacing, 0.)
-
-        self._carb1.p = vector(0., 0., 0.)
-        self._oxy1.p = vector(0., 0., 0.)
-        self._oxy2.p = vector(0., 0., 0.)
-
-        #self._oxy1pos0 = self._oxy1.pos.y
+        self._oxy1.reset(position=vector(spacing * cos(self._theta), spacing * sin(self._theta), 0.))
+        self._oxy2.reset(position=-1 * vector(spacing * cos(self._theta), spacing * sin(self._theta), 0.))
+        self._carb1.reset(position=vector(0., 0.0 * spacing, 0.))
 
     def update(self, E):
-        v1 = self._oxy1.pos - self._carb1.pos
-        v2 = self._oxy2.pos - self._carb1.pos
+        v1 = self._oxy1.pos() - self._carb1.pos()
+        v2 = self._oxy2.pos() - self._carb1.pos()
 
         angle = acos(dot(v1, -1.0 * v2) / (v1.mag * v2.mag))
 
         # Oxy1 forces
-        coulomb_force = self._oxy1.charge * E
+        coulomb_force = self._oxy1.coulomb(E)
         stretch_force = -1.0 * k * (mag(v1) - spacing) * norm(v1)
         torque_force = -1.0 * kt * spacing * angle * norm(v1 + v2)
-
-        self._oxy1.p += timestep * (coulomb_force + stretch_force + torque_force)
-        self._oxy1.pos += timestep * self._oxy1.p / self._oxy1.mass
+        self._oxy1.update(coulomb_force + stretch_force + torque_force, timestep)
 
         # Oxy2 forces
-        coulomb_force = self._oxy2.charge * E
+        coulomb_force = self._oxy2.coulomb(E)
         stretch_force = -1.0 * k * (mag(v2) - spacing) * norm(v2)
         torque_force = -1.0 * kt * spacing * angle * norm(v1 + v2)
-
-        self._oxy2.p += timestep * (coulomb_force + stretch_force + torque_force)
-        self._oxy2.pos += timestep * self._oxy2.p / self._oxy2.mass
+        self._oxy2.update(coulomb_force + stretch_force + torque_force, timestep)
 
         # Carb1 forces
-        coulomb_force = self._carb1.charge * E
+        coulomb_force = self._carb1.coulomb(E)
         stretch_force = k * (mag(v1) - spacing) * norm(v1) + k * (mag(v2) - spacing) * norm(v2)
         torque_force = 2.0 * kt * spacing * angle * norm(v1 + v2)
-
-        self._carb1.p += timestep * (coulomb_force + stretch_force + torque_force)
-        self._carb1.pos += timestep * self._carb1.p / self._carb1.mass
+        self._carb1.update(coulomb_force + stretch_force + torque_force, timestep)
 
         # Update springs
-        self._spring1.pos = self._carb1.pos
+        self._spring1.pos = self._carb1.pos()
         self._spring1.axis = v1
 
-        self._spring2.pos = self._carb1.pos
+        self._spring2.pos = self._carb1.pos()
         self._spring2.axis = v2
 
-carbon_atom = Carbon()
+    def pos(self):
+        return self._carb1.pos()
+
+carbon_dioxide = CarbonDioxide()
 
 evector = arrow(pos=vector(1.7 * spacing, 0., 0.), axis=vector(0., 0., 0.), shaftwidth=0.1 * spacing, color=color.magenta)
 
@@ -132,7 +133,7 @@ radio_1 = radio(text="8  ", name="8", bind=toggle_frequency, checked=True)
 radio_2 = radio(text="5.291  ", name="5.291", bind=toggle_frequency)
 radio_3 = radio(text="1.5  ", name="1.5", bind=toggle_frequency)
 radio_4 = radio(text="2.76  ", name="2.76", bind=toggle_frequency)
-_ = button(text="Reset", bind=carbon_atom.reset)
+_ = button(text="Reset", bind=carbon_dioxide.reset)
 
 time = 0
 flag = True
@@ -144,12 +145,12 @@ while 1:
     E = E0 * cos(time * frequency) * vector(0., 1., 0.)
     evector.axis = 0.5 * spacing * E / E0
 
-    if flag and (carbon_atom._carb1.pos.y < 0):
+    if flag and (carbon_dioxide.pos().y < 0):
         new_ = time
         flag = 0
         # print 1.0e-10/(new-old)
-    elif not flag and (carbon_atom._carb1.pos.y > 0):
+    elif not flag and (carbon_dioxide.pos().y > 0):
         flag = 1
         old = new_
 
-    carbon_atom.update(E)
+    carbon_dioxide.update(E)
