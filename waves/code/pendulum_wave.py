c9bbdd60f80c55f46ba47013feba584b5edb4d3c
Zeger Hendrikse <zegerh@yahoo.co.uk>
2025-03-25 13:35:55 +0100

    Update

--- waves/code/pendulum_wave.py
+++ waves/code/pendulum_wave.py
@@ -1,9 +1,12 @@
 #Web VPython 3.2
 
-from vpython import label, box, canvas, vec, color, cylinder, sphere, cos, sin, pi, rate, checkbox
+from vpython import label, box, canvas, vec, color, cylinder, sphere, cos, sin, pi, rate, checkbox, textures, slider
 
 title="""&#x2022; Based on <a href="https://glowscript.org/#/user/yizhe/folder/Public/program/PendulumWave">PendulumWave</a>
 &#x2022; Refactored by <a href="https://www.hendrikse.name/">Zeger Hendrikse</a> to <a href="https://github.com/zhendrikse/science/blob/main/waves/code/pendulum_wave.py">pendulum_wave.py</a>
+&#x2022; Related to <a href="https://www.hendrikse.name/science/waves/newtons_cradle.html">Newton&apos;s cradle</a> simulation
+
+&#x2022; $\\theta(t)=\\theta_0 \\cos \\bigg( \\sqrt{  \\dfrac {g} {L}} t \\bigg)$
 
 """
 
@@ -15,14 +18,20 @@ Tmax = Tpw / N
 Lmax = Tmax ** 2 * g / (4 * pi ** 2)
 width = Lmax
 total_balls = 20
-size = width / (2 * total_balls)
+size = .75 * (width / total_balls)
 
 
-display = canvas(title=title, width=600, background=color.gray(0.075), range=.5)
-display.camera.pos = vec(-1.5 * width, 0.5 * width, width)
+display = canvas(title=title, width=600, height=600, background=color.gray(0.075))
+display.camera.pos = 1.4 * vec(-1.5 * width, 0 * width, width)
 display.camera.axis = -display.camera.pos
-roof = box(pos=vec(0, (width + size) / 2, 0), size=vec(width, size, 0.5 * width), color=color.cyan)
-timer = label(pos=vec(width, width, 0) * .9, text="t =  s", space=50, height=24, font="monospace", box=False, color=color.yellow)
+
+floor = box(pos=vec(0, -.75 * (width + size) , 0), size=2 * vec(width, size, width), texture=textures.granite)
+roof = box(pos=vec(0, (width + size) / 2, 0), size=vec(width * 1.25, size, 0.5 * width), texture=textures.wood)
+cylinder(pos=vec(width * 1.2, width + size, -.4* width) / 2, axis=vec(0, -1.25, -.5), radius=-.02, texture=textures.wood)
+cylinder(pos=vec(width * 1.2, width + size, +.4* width) / 2, axis=vec(0, -1.25,  .5), radius=-.02, texture=textures.wood)
+cylinder(pos=vec(-width * 1.2, width + size, +.4* width) / 2, axis=vec(0, -1.25,  .5), radius=-.02, texture=textures.wood)
+cylinder(pos=vec(-width * 1.2, width + size, -.4* width) / 2, axis=vec(0, -1.25, -.5), radius=-.02, texture=textures.wood)
+timer = label(pos=vec(width, width, 0) * 1.5, text="t =  s", space=50, height=24, font="monospace", box=False, color=color.yellow)
 
 
 class Pendulum:
@@ -55,12 +64,20 @@ for i in range(total_balls):
 def toggle_clock(event):
     timer.visible = event.checked
 
-_ = checkbox(text="Clock", bind=toggle_clock, checked=True)
+fps = 500
+def animation_speed(event):
+    global fps
+    fps = event.value
 
+_ = checkbox(text="Clock", bind=toggle_clock, checked=True)
+display.append_to_caption("\n\nFrames per second")
+_ = slider(min=1, max=500, value=500, bind=animation_speed)
+
+#MathJax.Hub.Queue(["Typeset", MathJax.Hub])
 t = 0
 delta_t = 0.001
 while True:
-    rate(500)
+    rate(fps)
     for pendulum in pendulums:
         pendulum.update(delta_t)
     timer.text = "t = {:.1f} s".format(t)
