0e46d21577d0a4d0ccc3ffd4edbe165adf1dbc39
Zeger Hendrikse <zegerh@yahoo.co.uk>
2025-03-28 11:01:47 +0100

    Update

--- astrophysics/code/three_body.py
+++ astrophysics/code/three_body.py
@@ -1,47 +1,45 @@
 # Web VPython 3.2
 
-from vpython import *
+from vpython import sphere, rate, canvas, vector, color, sqrt
 
-caption = """ 
-&#x2022; Based on original by <a href="https://trinket.io/glowscript/9ece3648f0">Rhett Allain</a>
+title = """&#x2022; Based on original by <a href="https://trinket.io/glowscript/9ece3648f0">Rhett Allain</a>
 &#x2022; Belongs to <a href="https://www.youtube.com/watch?v=Ye2wIV8-SB8">this video</a> 
 &#x2022; Refactored by <a href="https://www.hendrikse.name/">Zeger Hendrikse</a>
 
+&#x2022; Click mouse to start animation
+
 """
 
-title = "<h3>Click mouse to start animation</h3>"
-
-animation = canvas(title=title, background=color.gray(0.075), caption=caption)
+animation = canvas(title=title, background=color.gray(0.075))
 
 G = 6.67e-11
 astronomical_unit = 1.49e11
-
+mass = 1e30
+rA = 0.1 * astronomical_unit
+rB = rA * 1 / 0.8
+vA = sqrt(G * 0.8 * mass * rA) / (rA + rB)
 
 class CelestialObject:
-    def __init__(self, position, velocity, mass, radius, color=color.yellow, make_trail=True):
-        self._sphere = sphere(pos=position, v=velocity, mass=mass, radius=radius, color=color, make_trail=make_trail)
+    def __init__(self, position, velocity, mass, radius, colour=color.yellow, make_trail=True):
+        self._sphere = sphere(pos=position, radius=radius, color=colour, make_trail=make_trail)
+        self._mass = mass
+        self._velocity = velocity
 
     def momentum(self):
-        return self._sphere.mass * self._sphere.v
+        return self._mass * self._velocity
 
     def distance_to(self, other):
         return other._sphere.pos - self._sphere.pos
 
     def force_between(self, other):
         radius = self.distance_to(other)
-        force_magnitude = G * self._sphere.mass * other._sphere.mass / mag(radius) ** 2
+        force_magnitude = G * self._mass * other._mass / (mag(radius) * mag(radius))
         force_vector = force_magnitude * norm(radius)
         return force_vector
 
     def move(self, force, dt):
-        self._sphere.v += force / self._sphere.mass * dt
-        self._sphere.pos += self._sphere.v * dt
-
-
-mass = 1e30
-rA = 0.1 * astronomical_unit
-rB = rA * 1 / 0.8
-vA = sqrt(G * 0.8 * mass * rA) / (rA + rB)
+        self._velocity += force / self._mass * dt
+        self._sphere.pos += self._velocity * dt
 
 sphere_A = CelestialObject(vector(rA, 0, 0), vector(0, vA, 0), mass, 190e7)
 sphere_B = CelestialObject(vector(-rB, 0, 0), vector(0, -vA / 0.8, 0), 0.8 * mass, 190e7, color.cyan)
