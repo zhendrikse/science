826ce9d56dd98e2355f0bd23ca03adb08056ba61
Zeger Hendrikse <zegerh@yahoo.co.uk>
2025-03-28 10:45:40 +0100

    Update

--- mathematics/code/numerical_methods.py
+++ mathematics/code/numerical_methods.py
@@ -1,6 +1,6 @@
 # Web VPython 3.2
 
-from vpython import *
+from vpython import box, helix, graph, vec, vector, sin, cos, sqrt, color, canvas, slider, mag, gcurve, rate, button
 
 title = """&#x2022; Based on <a href="https://trinket.io/glowscript/1fe7d9cdd6">this code</a> and <a href="https://www.youtube.com/watch?v=dShtlMl69kY">accompanying video</a>
 &#x2022; Refactored by <a href="https://www.hendrikse.name/">Zeger Hendrikse</a> to <a href="https://github.com/zhendrikse/science/blob/main/kinematics/code/numerical_methods.py">numerical_methods.py</a>
@@ -14,10 +14,9 @@ title = """&#x2022; Based on <a href="https://trinket.io/glowscript/1fe7d9cdd6">
 
 """
 
-scene = canvas(title=title, width=600, background=color.gray(0.075))
+display = canvas(title=title, width=600, background=color.gray(0.075), range=1.75, forward=vec(-.5, -.1, -.8))
 
-# Parameters
-amplitude = .5  # Amplitude
+amplitude = .5
 phi = 0.0  # Phase angle
 spring_rest_length = 1
 
@@ -68,6 +67,12 @@ class Oscillator:
             self._exact_time_lapse(dt)
         else:
             self._numerical_time_lapse(dt, num_method)
+
+    def reset(self):
+        self._velocity = 0
+        self._clock = 0
+        self._block.pos.x = amplitude
+        self._update_spring()
 
 
 position_graph = graph(title="Position vs Time", xtitle='Time', ytitle='Pos_x', background=color.black)
@@ -142,74 +147,86 @@ def rk4_step(x, v, dt, k, m):
 
 
 # GUI controls
-def set_dt(evt):
+dt = .05
+def set_dt(event):
     global dt
-    if evt.id == "dt":
-        dt = evt.value
-
+    dt = event.value
 
 running = False
-
-
-def run(button_):
+def toggle_run(button_):
     global running
     running = not running
     button_.text = "Stop" if running else "Run"
 
 
-scene.append_to_caption("\n")
+display.append_to_caption("\n")
 run_button = button(
     text="Run",
-    pos=scene.caption_anchor,
-    bind=run,
+    pos=display.caption_anchor,
+    bind=toggle_run,
 )
 
-scene.append_to_caption("   ")
+display.append_to_caption(" | Animation speed")
 dt_slider = slider(
     bind=set_dt,
-    max=0.5,
+    max=0.15,
     min=0.0001,
     step=0.0001,
-    value=0.1,
-    id="dt"
+    value=0.05
 )
-scene.append_to_caption("animation speed\n\n")
+display.append_to_caption("\n\n")
 
-temp = 0
-delta_t = 0.001
-time = 1000  # Number of steps
-while temp < 1000:
-    rate(24)
+def run(t=0, frame_rate=60):
+    global running
+    while t < 25 * frame_rate:
+        if not running:
+            rate(10)
+            continue
 
-    temp += delta_t
-    if running:
-        break
+        rate(frame_rate)
 
-# Run simulation
-t = 0
-dt = .05
-while t < time:
-    if not running:
+        exact_oscillator.time_lapse(dt)
+        euler_oscillator.time_lapse(dt, euler_step)
+        implicit_euler_oscillator.time_lapse(dt, implicit_euler_step)
+        runge_kutta_2_oscillator.time_lapse(dt, rk2_step)
+        runge_kutta_4_oscillator.time_lapse(dt, rk4_step)
+
+        exact_curve.plot(t, exact_oscillator.pos().x)
+        euler_curve.plot(t, euler_oscillator.pos().x)
+        implicit_curve.plot(t, implicit_euler_oscillator.pos().x)
+        rk2_curve.plot(t, runge_kutta_2_oscillator.pos().x)
+        rk4_curve.plot(t, runge_kutta_4_oscillator.pos().x)
+
+        euler_error.plot(t, abs(exact_oscillator.pos().x - euler_oscillator.pos().x))
+        implicit_error.plot(t, abs(exact_oscillator.pos().x - implicit_euler_oscillator.pos().x))
+        rk4_error.plot(t, abs(exact_oscillator.pos().x - runge_kutta_4_oscillator.pos().x))
+        rk2_error.plot(t, abs(exact_oscillator.pos().x - runge_kutta_2_oscillator.pos().x))
+
+        t += 1
+
+def reset():
+    exact_oscillator.reset()
+    euler_oscillator.reset()
+    implicit_euler_oscillator.reset()
+    runge_kutta_2_oscillator.reset()
+    runge_kutta_4_oscillator.reset()
+    display.range = 1.75
+    implicit_curve.delete()
+    exact_curve.delete()
+    euler_curve.delete()
+    rk2_curve.delete()
+    rk4_curve.delete()
+    euler_error.delete()
+    implicit_error.delete()
+    rk2_error.delete()
+    rk4_error.delete()
+
+while True:
+    run()
+    toggle_run(run_button)
+    while not running:
         rate(10)
-        continue
+    reset()
 
-    rate(60)
 
-    exact_oscillator.time_lapse(dt)
-    euler_oscillator.time_lapse(dt, euler_step)
-    implicit_euler_oscillator.time_lapse(dt, implicit_euler_step)
-    runge_kutta_2_oscillator.time_lapse(dt, rk2_step)
-    runge_kutta_4_oscillator.time_lapse(dt, rk4_step)
 
-    exact_curve.plot(t, exact_oscillator.pos().x)
-    euler_curve.plot(t, euler_oscillator.pos().x)
-    implicit_curve.plot(t, implicit_euler_oscillator.pos().x)
-    rk2_curve.plot(t, runge_kutta_2_oscillator.pos().x)
-    rk4_curve.plot(t, runge_kutta_4_oscillator.pos().x)
-
-    # euler_error.plot(t, abs(exact_setup['block'].pos.x - euler_setup['block'].pos.x))
-    # implicit_error.plot(t, abs(exact_setup['block'].pos.x - implicit_setup['block'].pos.x))
-    rk4_error.plot(t, abs(exact_oscillator.pos().x - runge_kutta_4_oscillator.pos().x))
-    rk2_error.plot(t, abs(exact_oscillator.pos().x - runge_kutta_2_oscillator.pos().x))
-
-    t += 1
