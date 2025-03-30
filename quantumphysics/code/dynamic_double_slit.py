26f52a5ec4c6ee3f6266bb09a8cc65ff71e55dcf
Zeger Hendrikse <zegerh@yahoo.co.uk>
2025-03-30 13:56:02 +0200

    Update

--- quantumphysics/code/dynamic_double_slit.py
+++ quantumphysics/code/dynamic_double_slit.py
@@ -1,5 +1,5 @@
 #Web VPython 3.2
-from vpython import ceil, pi, cos, rate, box, vec, canvas, color, arange, simple_sphere
+from vpython import ceil, pi, cos, rate, box, vec, canvas, color, arange, simple_sphere, label, checkbox
 
 title = """&#x2022; Original <a href="https://github.com/NelsonHackerman/Random_python_ideas/blob/main/double%20slit%20experiment.py">double slit experiment.py</a> by <a href="https://github.com/NelsonHackerman">Nelson Hackerman</a>
 &#x2022; Ported from PyGame to Glowscript by <a href="https://www.hendrikse.name/">Zeger Hendrikse</a>, see <a href="https://github.com/zhendrikse/science/blob/main/quantumphysics/code/dynamic_double_slit.py">dynamic_double_slit.py</a>
@@ -36,7 +36,7 @@ def colour(x, range_):
     return vec(red, 50, blue) / 255
 
 
-def initialize_simulation():
+def initialize_simulation(pixels_are_round):
     x = arange(0, 13, dx)
     y = arange(0, 7, dy)
     const1 = [1 for _ in range(len(x))]
@@ -48,77 +48,97 @@ def initialize_simulation():
     for i in range(len(x)):
         temp = []
         for j in range(len(y)):
-            # temp.append(box(pos=vec(xmesh[j][i] * 100 + 50, ymesh[j][i] * 100 + 50, 0), size=vec(ceil(100 * dx), ceil(100 * dy), 0)))
-            temp.append(
-                simple_sphere(pos=vec(xmesh[j][i] * 100 + 50, ymesh[j][i] * 100 + 50, 0), radius=ceil(100 * dx)))
+            if pixels_are_round:
+                temp.append(simple_sphere(pos=vec(xmesh[j][i] * 100 + 50, ymesh[j][i] * 100 + 50, 0), radius=ceil(100 * dx)))
+            else:
+                temp.append(box(pos=vec(xmesh[j][i] * 100 + 50, ymesh[j][i] * 100 + 50, 0), size=vec(ceil(100 * dx), ceil(100 * dy), 0)))
+
         pixels.append(temp)
 
     return pixels
 
+def initialize_meshes(len_x, len_y):
+    umesh = outer_product([0 for _ in range(len_y)], arange(0, 13, dx))
+    utmesh = [[0. for _ in range(len_x)] for _ in range(len_y)]
+    uttmesh = [[0. for _ in range(len_x)] for _ in range(len_y)]
+    return umesh, utmesh, uttmesh
 
-pixels = initialize_simulation()
-umesh = outer_product([0 for _ in range(len(pixels[0]))], arange(0, 13, dx))
-utmesh = [[0. for _ in range(len(pixels))] for _ in range(len(pixels[0]))]
-uttmesh = [[0. for _ in range(len(pixels))] for _ in range(len(pixels[0]))]
-
-t = 0
+# Calculated once and for all, for enhancing performance
 velocity_source_2 = velocity_source * velocity_source
 dx_2 = dx * dx
 dy_2 = dy * dy
 pi_2 = pi / 2
-while True:
-    t += dt
-    rate(100)
 
-    for n in range(1, len(pixels) - 1):
-        for m in range(1, len(pixels[0]) - 1):
-            uttmesh[m][n] = velocity_source_2 * ((umesh[m][n - 1] + umesh[m][n + 1] - 2 * umesh[m][n]) / dx_2 + (
-                        umesh[m - 1][n] + umesh[m + 1][n] - 2 * umesh[m][n]) / dy_2)
+def run_simulation(umesh, utmesh, uttmesh, pixels):
+    t = 0
+    while True:
+        t += dt
+        rate(100)
 
-    for n in range(1, len(pixels) - 1):
-        for m in range(1, len(pixels[0]) - 1):
-            utmesh[m][n] += uttmesh[m][n] * dt
-            umesh[m][n] += utmesh[m][n] * dt
+        for n in range(1, len(pixels) - 1):
+            for m in range(1, len(pixels[0]) - 1):
+                uttmesh[m][n] = velocity_source_2 * ((umesh[m][n - 1] + umesh[m][n + 1] - 2 * umesh[m][n]) / dx_2 + (
+                            umesh[m - 1][n] + umesh[m + 1][n] - 2 * umesh[m][n]) / dy_2)
 
-    # boundary conditions
-    umesh[:][0] = umesh[:][1]
-    umesh[:][-1] = umesh[:][-2]
-    umesh[0][:] = umesh[1][:]
-    umesh[-1][:] = umesh[-2][:]
+        for n in range(1, len(pixels) - 1):
+            for m in range(1, len(pixels[0]) - 1):
+                utmesh[m][n] += uttmesh[m][n] * dt
+                umesh[m][n] += utmesh[m][n] * dt
 
-    if t < pi_2:
-        for n in range(len(pixels[0])):
-            umesh[n][0] = -cos(t * 20) + 1
+        # boundary conditions
+        umesh[:][0] = umesh[:][1]
+        umesh[:][-1] = umesh[:][-2]
+        umesh[0][:] = umesh[1][:]
+        umesh[-1][:] = umesh[-2][:]
 
-    umesh[0][0] = umesh[1][1]
-    umesh[0][-1] = umesh[1][-2]
-    umesh[-1][0] = umesh[-2][1]
-    umesh[-1][-1] = umesh[-2][-2]
+        if t < pi_2:
+            for n in range(len(pixels[0])):
+                umesh[n][0] = -cos(t * 20) + 1
 
-    for n in range(inter1):
-        umesh[n][wall_x] = 69
-        umesh[n][wall_x - 1] = umesh[n][wall_x - 2]
-        umesh[n][wall_x + 1] = umesh[n][wall_x + 2]
-    umesh[inter1][wall_x] = (umesh[inter1][wall_x - 1] + umesh[inter1][wall_x + 1] + umesh[inter1 + 1][wall_x]) / 3
+        umesh[0][0] = umesh[1][1]
+        umesh[0][-1] = umesh[1][-2]
+        umesh[-1][0] = umesh[-2][1]
+        umesh[-1][-1] = umesh[-2][-2]
 
-    for n in range(mid_up, mid_down):
-        umesh[n][wall_x] = 69
-        umesh[n][wall_x - 1] = umesh[n][wall_x - 2]
-        umesh[n][wall_x + 1] = umesh[n][wall_x + 2]
-    umesh[mid_up - 1][wall_x] = (umesh[mid_up - 1][wall_x - 1] + umesh[mid_up - 1][wall_x + 1] + umesh[mid_up - 2][
-        wall_x]) / 3
-    umesh[mid_down][wall_x] = (umesh[mid_down][wall_x - 1] + umesh[mid_down][wall_x + 1] + umesh[mid_down + 1][
-        wall_x]) / 3
+        for n in range(inter1):
+            umesh[n][wall_x] = 69
+            umesh[n][wall_x - 1] = umesh[n][wall_x - 2]
+            umesh[n][wall_x + 1] = umesh[n][wall_x + 2]
+        umesh[inter1][wall_x] = (umesh[inter1][wall_x - 1] + umesh[inter1][wall_x + 1] + umesh[inter1 + 1][wall_x]) / 3
 
-    for n in range(inter2, len(pixels[0])):
-        umesh[n][wall_x] = 69
-        umesh[n][wall_x - 1] = umesh[n][wall_x - 2]
-        umesh[n][wall_x + 1] = umesh[n][wall_x + 2]
-    umesh[-1][wall_x] = 69
-    umesh[-1][wall_x - 1] = umesh[-1][wall_x - 2]
-    umesh[-1][wall_x + 1] = umesh[-1][wall_x + 2]
-    umesh[inter2][wall_x] = (umesh[inter2][wall_x - 1] + umesh[inter2][wall_x + 1] + umesh[inter2 - 1][wall_x]) / 3
+        for n in range(mid_up, mid_down):
+            umesh[n][wall_x] = 69
+            umesh[n][wall_x - 1] = umesh[n][wall_x - 2]
+            umesh[n][wall_x + 1] = umesh[n][wall_x + 2]
+        umesh[mid_up - 1][wall_x] = (umesh[mid_up - 1][wall_x - 1] + umesh[mid_up - 1][wall_x + 1] + umesh[mid_up - 2][
+            wall_x]) / 3
+        umesh[mid_down][wall_x] = (umesh[mid_down][wall_x - 1] + umesh[mid_down][wall_x + 1] + umesh[mid_down + 1][
+            wall_x]) / 3
 
-    for i in range(len(pixels)):
-        for j in range(len(pixels[0])):
-            pixels[i][j].color = vec(0, 0, 0) if umesh[j][i] == 69 else colour(umesh[j][i] - floor, colorrange)
+        for n in range(inter2, len(pixels[0])):
+            umesh[n][wall_x] = 69
+            umesh[n][wall_x - 1] = umesh[n][wall_x - 2]
+            umesh[n][wall_x + 1] = umesh[n][wall_x + 2]
+        umesh[-1][wall_x] = 69
+        umesh[-1][wall_x - 1] = umesh[-1][wall_x - 2]
+        umesh[-1][wall_x + 1] = umesh[-1][wall_x + 2]
+        umesh[inter2][wall_x] = (umesh[inter2][wall_x - 1] + umesh[inter2][wall_x + 1] + umesh[inter2 - 1][wall_x]) / 3
+
+        for i in range(len(pixels)):
+            for j in range(len(pixels[0])):
+                pixels[i][j].color = vec(0, 0, 0) if umesh[j][i] == 69 else colour(umesh[j][i] - floor, colorrange)
+
+use_round_pixels = True
+def toggle_square_pixels(event):
+    global use_round_pixels
+    use_round_pixels = event.checked
+
+display.append_to_caption("\nRound pixels ")
+_ = checkbox(bind=toggle_square_pixels, checked=use_round_pixels)
+
+popup = label(pos=vec(700, 350, 0), text="Click mouse to start", height=25, box=False, color=color.yellow)
+display.waitfor("click")
+popup.visible = False
+pixels = initialize_simulation(use_round_pixels)
+u_mesh, ut_mesh, utt_mesh = initialize_meshes(len(pixels), len(pixels[0]))
+run_simulation(u_mesh, ut_mesh, utt_mesh, pixels)
