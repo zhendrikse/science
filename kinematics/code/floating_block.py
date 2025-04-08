18254dd9b80475093807cc8b82eff60177afb0c3
Zeger Hendrikse <zegerh@yahoo.co.uk>
2025-04-08 10:53:39 +0200

    Update

--- kinematics/code/floating_block.py
+++ kinematics/code/floating_block.py
@@ -7,29 +7,70 @@ title = """&#x2022; Based on original <a href="https://github.com/gcschmit/vpyth
 
 """
 
-animation = canvas(title=title, background=color.gray(0.075), forward=vector(-0.52, -0.67, -0.52), range=2.)
+display = canvas(title=title, background=color.gray(0.075), forward=vector(-0.52, -0.67, -0.52), range=2., width=600, height=600)
+
+class WoodenBlock:
+    def __init__(self, density=500):
+        self._block = box(pos=vector(0, 0, 0), texture=textures.wood, size=vector(0.4, 0.4, 0.1))
+        self._density = density
+        self._velocity = vector(0, 0, 0)
+
+    def pos(self):
+        return self._block.pos
+
+    def buoyancy_force(self, water):
+        return water.density() * -g * self._submerged_volume(water)
+
+    def drag_force(self):
+        return dragCoefficient * self._velocity.y
+
+    def net_force_in(self, water):
+        gravitational_force = self._mass() * g
+        return self.buoyancy_force(water) + gravitational_force + self.drag_force()
+
+    def _mass(self):
+        return self._density * self._block.size.x * self._block.size.y * self._block.size.z
+
+    def _submerged_volume(self, water: box):
+        top_of_fluid = water.pos().y + water.size().y / 2
+        top_of_object = self.pos().y + self._block.size.y / 2
+        bottom_of_object = self.pos().y - self._block.size.y / 2
+
+        if top_of_object <= top_of_fluid:
+            height_submerged = self._block.size.y
+        elif bottom_of_object >= top_of_fluid:
+            height_submerged = 0
+        else:
+            height_submerged = (top_of_fluid - bottom_of_object)
+
+        return self._block.size.x * height_submerged * self._block.size.z
+
+    def time_lapse(self, dt):
+        acceleration = net_force / self._mass()
+        self._velocity += + acceleration * dt
+        self._block.pos += self._velocity * dt
+
+class Liquid:
+    def __init__(self, density=1000):
+        self._liquid = box(size=vector(2, 2, 0.75), color=color.blue, opacity=0.3)
+        self._density = density
+
+    def density(self):
+        return self._density
+
+    def size(self):
+        return self._liquid.size
+
+    def pos(self):
+        return self._liquid.pos
 
 
-def submerged_volume(wooden_block:box, water:box):
-    top_of_fluid = water.pos.y + water.size.y / 2
-    top_of_object = wooden_block.pos.y + wooden_block.size.y / 2
-    bottom_of_object = wooden_block.pos.y - wooden_block.size.y / 2
+wooden_block = WoodenBlock()
+fluid = Liquid()
 
-    if top_of_object <= top_of_fluid:
-        height_submerged = wooden_block.size.y
-    elif bottom_of_object >= top_of_fluid:
-        height_submerged = 0
-    else:
-        height_submerged = (top_of_fluid - bottom_of_object)
-
-    return wooden_block.size.x * height_submerged * wooden_block.size.z
-
-
-fluid = box(size=vector(2, 2, 0.75), color=color.blue, opacity=0.3, density=1000)
-floating_object = box(pos=vector(0, 0, 0), v=vector(0, 0, 0), texture=textures.wood, density=500, size=vector(0.4, 0.4, 0.1))
-
-graphs = graph(title="Buyoancy")
-buoyance_curve = gcurve(color=color.magenta)
+display.append_to_caption("\n\n")
+graphs = graph(title="Buoyancy (magenta) and drag (blue) forces", background=color.black, xtitle="Time", ytitle="Force")
+buoyancy_curve = gcurve(color=color.magenta)
 drag_force_curve = gcurve(color=color.blue)
 
 dragCoefficient = vector(0, -5.0, 0)
@@ -37,21 +78,13 @@ g = vector(0, -9.8, 0)
 
 t = 0
 dt = 0.001
-while t < 20 and floating_object.pos.y > (fluid.pos.y - fluid.size.y / 2):
+while t < 20 and wooden_block.pos().y > (fluid.pos().y - fluid.size().y / 2):
     rate(1 / dt)
 
-    mass = floating_object.density * floating_object.size.x * floating_object.size.y * floating_object.size.z
+    net_force = wooden_block.net_force_in(fluid)
+    wooden_block.time_lapse(dt)
 
-    gravitational_force = mass * g
-    drag_force = dragCoefficient * floating_object.v.y
-    buoyancy_force = fluid.density * -g * submerged_volume(floating_object, fluid)
-
-    net_force = buoyancy_force + gravitational_force + drag_force
-    acceleration = net_force / mass
-    floating_object.v += + acceleration * dt
-    floating_object.pos += floating_object.v * dt
-
-    buoyance_curve.plot(t, buoyancy_force.y)
-    drag_force_curve.plot(t, drag_force.y)
+    buoyancy_curve.plot(t, wooden_block.buoyancy_force(fluid).y)
+    drag_force_curve.plot(t, wooden_block.drag_force().y)
 
     t += dt
