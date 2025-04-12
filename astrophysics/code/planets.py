e98abfaf638c669245ee0c2781a4fc03b4c5de2c
Zeger Hendrikse <zegerh@yahoo.co.uk>
2025-04-12 22:32:53 +0200

    Update

--- astrophysics/code/planets.py
+++ astrophysics/code/planets.py
@@ -6,7 +6,9 @@ title = """&#x2022; <a href="https://www.glowscript.org/#/user/PHYS172x/folder/M
 
 """
 
-animation = canvas(background=color.gray(0.075), range=19, center=vector(-4.8, 3.55, 0), title=title)
+display_range = 22
+display_center = vector(-6.5, 3.55, 0)
+animation = canvas(width=600, height=450, background=color.gray(0.075), range=display_range, center=display_center, title=title)
 sun_textures = [
     "https://i.imgur.com/yoEzbtg.jpeg",
     "https://www.hendrikse.name/science/astrophysics/images/textures/sun.jpg",
@@ -35,9 +37,10 @@ class Mercury(Planet):
             "https://upload.wikimedia.org/wikipedia/commons/3/30/Mercury_in_color_-_Prockter07_centered.jpg"
         ]
         Planet.__init__(self, vector(-18, 9, 0), textures_, 0.00350)
-        label(pos=vector(-18, 3.5, 0), text='Mercury')
-        label(pos=vector(-18, 15, 0), text='θ = 0°', box=False)
-        label(pos=vector(-18, 13.5, 0), text='58d 15.5h', box=False)
+        label(pos=vector(-18, 4, 0), text='Mercury')
+        label(pos=vector(-18, 17.5, 0), text='θ = 0°', box=False)
+        label(pos=vector(-18, 15.5, 0), text='58d 15.5h', box=False)
+        label(pos=vector(-18, 13.5, 0), text='167℃', box=False)
 
 
 class Venus(Planet):
@@ -50,9 +53,10 @@ class Venus(Planet):
         Planet.__init__(self, vector(-6, 9, 0), textures_, 0.00244)
         self._sphere.opacity=0.7
         self._sphere.emissive=True
-        label(pos=vector(-6, 3.5, 0), text='Venus')
-        label(pos=vector(-6, 15, 0), text='θ = 177.3°', box=False)
-        label(pos=vector(-6, 13.5, 0), text='243d 26m', box=False)
+        label(pos=vector(-6, 4, 0), text='Venus')
+        label(pos=vector(-6, 17.5, 0), text='θ = 177.3°', box=False)
+        label(pos=vector(-6, 15.5, 0), text='243d 26m', box=False)
+        label(pos=vector(-6, 13.5, 0), text='464℃', box=False)
 
 class Earth(Planet):
     def __init__(self):
@@ -62,9 +66,10 @@ class Earth(Planet):
             "https://www.hendrikse.name/science/astrophysics/images/textures/earth.jpg"
         ]
         Planet.__init__(self, vector(6, 9, 0), textures_, 0.0408)
-        label(pos=vector(6, 3.5, 0), text='Earth')
-        label(pos=vector(6, 15, 0), text='θ = 23.4°', box=False)
-        label(pos=vector(6, 13.5, 0), text='23h 56m', box=False)
+        label(pos=vector(6, 4, 0), text='Earth')
+        label(pos=vector(6, 17.5, 0), text='θ = 23.4°', box=False)
+        label(pos=vector(6, 15.5, 0), text='23h 56m', box=False)
+        label(pos=vector(6, 13.5, 0), text='15℃', box=False)
 
 class Mars(Planet):
     def __init__(self):
@@ -76,9 +81,10 @@ class Mars(Planet):
         Planet.__init__(self, vector(18, 9, 0), textures_, 0.0439)
         self._sphere.opacity = 0.7
         self._sphere.emissive = True
-        label(pos=vector(18, 3.5, 0), text='Mars')
-        label(pos=vector(18, 15, 0), text='θ = 25.2°', box=False)
-        label(pos=vector(18, 13.5, 0), text='1d 36m', box=False)
+        label(pos=vector(18, 4, 0), text='Mars')
+        label(pos=vector(18, 17.5, 0), text='θ = 25.2°', box=False)
+        label(pos=vector(18, 15.5, 0), text='1d 36m', box=False)
+        label(pos=vector(18, 13.5, 0), text='-65℃', box=False)
 
 class Jupiter(Planet):
     def __init__(self):
@@ -87,12 +93,13 @@ class Jupiter(Planet):
             "https://www.hendrikse.name/science/astrophysics/images/textures/jupiter.jpg",
             "https://upload.wikimedia.org/wikipedia/commons/e/e2/Jupiter.jpg"
         ]
-        Planet.__init__(self, vector(-18, -7, 0), textures_, 0.0541)
+        Planet.__init__(self, vector(-18, -10, 0), textures_, 0.0541)
         self._sphere.opacity = 0.7
         self._sphere.emissive = True
-        label(pos=vector(-19, -12.5, 0), text='Jupiter')
+        label(pos=vector(-19, -15.5, 0), text='Jupiter')
         label(pos=vector(-19, -1, 0), text='θ = 3.1°', box=False)
-        label(pos=vector(-19, -2.5, 0), text='9h 55m', box=False)
+        label(pos=vector(-19, -3, 0), text='9h 55m', box=False)
+        label(pos=vector(-19, -5, 0), text='-110℃', box=False)
 
 class Saturn(Planet):
     def __init__(self):
@@ -101,17 +108,18 @@ class Saturn(Planet):
             "https://www.hendrikse.name/science/astrophysics/images/textures/saturn.jpg",
             "https://upload.wikimedia.org/wikipedia/commons/b/b4/Saturn_%28planet%29_large.jpg"
         ]
-        Planet.__init__(self, vector(-6, -7, 0), textures_, 0.0640)
+        Planet.__init__(self, vector(-6, -10, 0), textures_, 0.0640)
         self._sphere.opacity = 0.7
         self._sphere.emissive = True
-        label(pos=vector(-6, -12.5, 0), text='Saturn')
+        label(pos=vector(-6, -15.5, 0), text='Saturn')
         label(pos=vector(-6, -1, 0), text='θ = 26.7°', box=False)
-        label(pos=vector(-6, -2.5, 0), text='10h 40m', box=False)
-        saturn_ring_1 = ring(pos=vector(-6, -7, 0), axis=vector(2, -3, 0), radius=6, thickness=0.09,
+        label(pos=vector(-6, -3, 0), text='10h 40m', box=False)
+        label(pos=vector(-6, -5, 0), text='-140℃', box=False)
+        saturn_ring_1 = ring(pos=vector(-6, -10, 0), axis=vector(2, -3, 0), radius=6, thickness=0.09,
                              texture="https://upload.wikimedia.org/wikipedia/commons/b/b4/Saturn_%28planet%29_large.jpg")
-        saturn_ring_2 = ring(pos=vector(-6, -7, 0), axis=vector(2, -3, 0), radius=6.5, thickness=0.07,
+        saturn_ring_2 = ring(pos=vector(-6, -10, 0), axis=vector(2, -3, 0), radius=6.5, thickness=0.07,
                              texture="https://upload.wikimedia.org/wikipedia/commons/b/b4/Saturn_%28planet%29_large.jpg")
-        saturn_ring_3 = ring(pos=vector(-6, -7, 0), axis=vector(2, -3, 0), radius=5.5, thickness=0.07,
+        saturn_ring_3 = ring(pos=vector(-6, -10, 0), axis=vector(2, -3, 0), radius=5.5, thickness=0.07,
                              texture="https://upload.wikimedia.org/wikipedia/commons/b/b4/Saturn_%28planet%29_large.jpg")
 
 class Uranus(Planet):
@@ -121,10 +129,11 @@ class Uranus(Planet):
             "https://www.hendrikse.name/science/astrophysics/images/textures/uranus.jpg",
             "https://upload.wikimedia.org/wikipedia/commons/3/3d/Uranus2.jpg"
         ]
-        Planet.__init__(self, vector(6, -7, 0), textures_, 0.0750)
-        label(pos=vector(6, -12.5, 0), text='Uranus')
+        Planet.__init__(self, vector(6, -10, 0), textures_, 0.0750)
+        label(pos=vector(6, -15.5, 0), text='Uranus')
         label(pos=vector(6, -1, 0), text='θ = 97.8°', box=False)
-        label(pos=vector(6, -2.5, 0), text='17h 14m', box=False)
+        label(pos=vector(6, -3, 0), text='17h 14m', box=False)
+        label(pos=vector(6, -5, 0), text='-195℃', box=False)
 
 class Neptune(Planet):
     def __init__(self):
@@ -133,12 +142,13 @@ class Neptune(Planet):
             "https://www.hendrikse.name/science/astrophysics/images/textures/neptune.jpg",
             "https://upload.wikimedia.org/wikipedia/commons/5/56/Neptune_Full.jpg"
         ]
-        Planet.__init__(self, vector(18, -7, 0), textures_, 0.0490)
+        Planet.__init__(self, vector(18, -10, 0), textures_, 0.0490)
         self._sphere.opacity = 0.7
         self._sphere.emissive = True
-        label(pos=vector(18, -12.5, 0), text='Neptune')
+        label(pos=vector(18, -15.5, 0), text='Neptune')
         label(pos=vector(18, -1, 0), text='θ = 28.3°', box=False)
-        label(pos=vector(18, -2.5, 0), text='16h', box=False)
+        label(pos=vector(18, -3, 0), text='16h', box=False)
+        label(pos=vector(18, -5, 0), text='-200℃', box=False)
 
 def zoom_in_on(selected_object):
     if not selected_object:
@@ -157,8 +167,8 @@ def on_mouse_click():
 animation.bind('click', on_mouse_click)
 
 def on_reset():
-    animation.center = vector(-4.8, 3.55, 0)
-    animation.range = 19
+    animation.center = display_center
+    animation.range = display_range
 
 def toggle_textures(event):
     if event.name == "imgur":
