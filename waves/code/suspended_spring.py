4ee8fb3bfde54674a6f163b8bb66a1ae8b239432
Zeger Hendrikse <zegerh@yahoo.co.uk>
2025-04-15 17:22:22 +0200

    Update

--- waves/code/suspended_spring.py
+++ waves/code/suspended_spring.py
@@ -1,17 +1,18 @@
-#Web VPython 3.2
+# Web VPython 3.2
 
-from vpython import rate, vec, canvas, helix, color, cylinder, textures, box, button, slider
+from vpython import rate, vec, canvas, helix, color, cylinder, textures, box, button, slider, checkbox
 
-title = """
-&#x2022; Spring implementation based on <a href="https://bphilhour.trinket.io/physics-through-glowscript-an-introductory-course#/1-introduction-objects-parameters-and-the-3d-environment/optional-compound-objects">this example</a> from tutorial by B. Philhour
+title = """&#x2022; Spring implementation based on <a href="https://bphilhour.trinket.io/physics-through-glowscript-an-introductory-course#/1-introduction-objects-parameters-and-the-3d-environment/optional-compound-objects">this example</a> from tutorial by B. Philhour
 &#x2022; Refactored and modified by <a href="https://www.hendrikse.name/">Zeger Hendrikse</a> to <a href="https://github.com/zhendrikse/science/blob/main/waves/code/hanging_spring.py">hanging_spring.py</a>
 
 """
 
-display = canvas(title=title, background=color.gray(0.075), width=600, height=500, forward=vec(0, -.35, -1), range=.2, center=vec(0, -.14, 0))
+display = canvas(title=title, background=color.gray(0.075), width=600, height=500, forward=vec(0, -.35, -1), range=.2,
+                 center=vec(0, -.14, 0))
 
 g = 9.8
 frame_rate = 100
+
 
 class SpringElement:
     def __init__(self, position, axis, radius, mass, length):
@@ -56,36 +57,35 @@ class SpringElement:
 
 
 class Spring:
-    def __init__(self, N, mass, k, drag, topPos, botPos, width):
+    def __init__(self, N, mass, k, drag, left_pos, right_pos, width):
         self._elements = []
         self._drag = drag
         self._k = k
-        self._topPos = topPos
-        self._botPos = botPos
+        self._left_pos = left_pos
+        self._right_pos = right_pos
 
-        line = botPos - topPos
-        element_length = (topPos - botPos).mag / N
+        line = right_pos - left_pos
+        element_length = (left_pos - right_pos).mag / N
         for i in range(N):
-            self._elements.append(SpringElement(topPos + (i / N) * line, element_length * line.norm(), width / 2, mass / N, element_length))
+            self._elements.append(
+                SpringElement(left_pos + (i / N) * line, element_length * line.norm(), width / 2, mass / N,
+                              element_length))
 
     def update_by(self, dt):
         total_elements = len(self._elements)
         for i in range(total_elements):
             self._elements[i].update_gravitational_force(self._drag)
 
-        self._elements[0].update_left_element(self._elements[1], self._k, self._topPos)
+        self._elements[0].update_left_element(self._elements[1], self._k, self._left_pos)
         tension = 0.0
         for i in range(1, total_elements - 1):
             tension += self._elements[i].update_with(self._elements[i - 1], self._elements[i + 1], self._k, dt)
-        self._elements[-1].update_right_element(self._elements[-2], self._k, self._botPos)
+        self._elements[-1].update_right_element(self._elements[-2], self._k, self._right_pos)
 
         return tension / total_elements
 
     def force_on_last_element(self):
         return self._elements[-1].force()
-
-    def update_with(self, cart_position):
-        self._botPos = cart_position
 
     def set_k_to(self, event):
         self._k = event.value
@@ -93,32 +93,57 @@ class Spring:
     def set_drag_to(self, event):
         self._drag = event.value
 
+    def set_left_height_to(self, event):
+        self._left_pos.y = event.value
+
+    def set_right_height_to(self, event):
+        self._right_pos.y = event.value
+
+
 def set_scene():
     cylinder(pos=vec(-.17, 0.02, 0), radius=0.005, texture=textures.wood, axis=vec(0, -.25, 0))
     cylinder(pos=vec(+.17, 0.02, 0), radius=0.005, texture=textures.wood, axis=vec(0, -.25, 0))
-    cylinder(pos=vec(+.17, 0.0025, 0), radius=0.0025, texture=textures.wood, axis=vec(-0.02, 0, 0))
-    cylinder(pos=vec(-.17, 0.0025, 0), radius=0.0025, texture=textures.wood, axis=vec(+0.02, 0, 0))
+    left = cylinder(pos=vec(-.17, 0.0025, 0), radius=0.0025, texture=textures.wood, axis=vec(+0.02, 0, 0))
+    right = cylinder(pos=vec(+.17, 0.0025, 0), radius=0.0025, texture=textures.wood, axis=vec(-0.02, 0, 0))
     box(pos=vec(0, -0.23, 0), size=vec(0.4, 0.005, 0.2), texture=textures.rug)
+    return left, right
+
 
 def reset(event):
-    global spring
+    global spring, left, right
     for obj in display.objects:
         obj.visible = False
         del obj
-    set_scene()
-    spring = Spring(25, 0.1, 100, 0.025, vec(-.15, 0, 0), vec(.15, 0, 0), 0.025)
+    left, right = set_scene()
+    spring = Spring(30, 0.1, 100, 0.025, vec(-.15, 0, 0), vec(.15, 0, 0), 0.025)
+
+
+def toggle_gravity(event):
+    global g
+    g = 9.8 if event.checked else 0
+
 
 def set_frame_rate(event):
     global frame_rate
     frame_rate = event.value
 
+
+def modify_height_left(event):
+    spring.set_left_height_to(event)
+    left.pos.y = event.value + 0.0025
+
+
+def modify_height_right(event):
+    spring.set_right_height_to(event)
+    right.pos.y = event.value + 0.0025
+
+
 spring = Spring(25, 0.1, 100, 0.025, vec(-.15, 0, 0), vec(.15, 0, 0), 0.025)
 
-display.append_to_caption("\n")
-_ = button(text="reset", bind=reset)
-
-display.append_to_caption("\n\nSpring constant")
+display.append_to_caption("\nSpring constant")
 _ = slider(min=100, max=1000, value=100, bind=spring.set_k_to)
+display.append_to_caption("\t")
+_ = button(text="reset", bind=reset)
 
 display.append_to_caption("\n\nDrag")
 _ = slider(min=0, max=0.05, value=0.025, bind=spring.set_drag_to)
@@ -126,8 +151,17 @@ _ = slider(min=0, max=0.05, value=0.025, bind=spring.set_drag_to)
 display.append_to_caption("\n\nFrame rate")
 _ = slider(min=0, max=200, value=frame_rate, bind=set_frame_rate)
 
+display.append_to_caption("\n\nHeight left")
+_ = slider(min=-.1, max=0, value=0, bind=modify_height_left)
+
+display.append_to_caption("\n\nHeight right")
+_ = slider(min=-.1, max=0, value=0, bind=modify_height_right)
+
+display.append_to_caption("\n\n")
+_ = checkbox(text="Gravity ", checked=True, bind=toggle_gravity)
+
 delta_t = 0.001
-set_scene()
+left, right = set_scene()
 while True:
     rate(frame_rate)
     spring.update_by(delta_t)
