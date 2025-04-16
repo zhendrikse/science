feb9141a6230b9c38a6c313261675e622b45f329
Zeger Hendrikse <zegerh@yahoo.co.uk>
2025-04-16 14:59:45 +0200

    Update

--- nature/code/fire.py
+++ nature/code/fire.py
@@ -8,7 +8,7 @@ title = """&#x2022; Based on <a href="https://github.com/beltoforion/recreationa
 
 dim_x, dim_y = 100, 70
 display = canvas(title=title, range=32, background=color.gray(0.075), width=600, height=400, center=.5 * vec(dim_x, dim_y - 4, 0))
-MathJax.Hub.Queue(["Typeset", MathJax.Hub])
+#MathJax.Hub.Queue(["Typeset", MathJax.Hub])
 
 def init():
     cells = [[0. for _ in range(dim_y)] for _ in range(dim_x)]
@@ -20,7 +20,7 @@ def init():
     fire_colors = []
     for i in range(256):
         colour = p1 + (p2 - p1) * i / 128 if i < 128 else p2 + (p3 - p2) * (i - 128) / 128
-        fire_colors.append(colour)
+        fire_colors.append(colour / 255)
 
     return cells, fire_colors
 
@@ -30,14 +30,14 @@ def update(pixels, cells, fire_colors):
 
     for r in range(0, dim_y - 1):
         for c in range(0, dim_x - 1):
-            if r == 0 and 5 < c < dim_x-5:
+            if r == 0 and 5 < c < dim_x - 5:
                 cells[c][r] = cells[c][1] + random() * 0.9
             else:
                 intensity = 4.1 + 0.3 * (abs(c - dim_x / 2) / (dim_x / 2))
                 cells[c][r] = (old[c - 1][r - 1] + old[c][r - 1] + old[c + 1][r - 1] + old[c][r - 2]) / intensity
 
             val = min(int(255 * cells[c][r - 1]), 255) * (r > 2)
-            pixels[c][r].color  = fire_colors[val] / 255
+            pixels[c][r].color  = fire_colors[val]
 
 
 def main():
@@ -45,7 +45,7 @@ def main():
     cells, fire_colors = init()
 
     while True:
-        rate(10)
+        rate(60)
         update(pixels, cells, fire_colors)
 
 
